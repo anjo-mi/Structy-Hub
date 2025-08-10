46ac1437a1889986978496ef7547e946f2cee9cd
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-10 18:25:10 -0400

    Solved 'leaf list' (node)

---
+++ Binary-Tree-I/067-leaf-list/leaf-list.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const leafList = (root) => {
+  if (!root) return [];
+  const leaves = [];
+  const stack = [root];
+  while (stack.length){
+    const curr = stack.pop();
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
+    if (!curr.left && !curr.right) leaves.push(curr.val);
+  }
+  return leaves;
+};
+/*
+
+*/
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
+console.log(leafList(a), [ 'd', 'g', 'h' ])
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /       \
+//   g         h
+
+
+module.exports = {
+  leafList,
+};
