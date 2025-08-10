8abfd1f582d6afe5f0e054fe915430b96f37b2b6
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-10 17:23:33 -0400

    Solved 'tree path finder' (node)

---
+++ Binary-Tree-I/059-tree-path-finder/tree-path-finder.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const pathFinder = (root, target) => {
+  if (!root) return null;
+  const path = [root.val];
+  if (root.val === target) return path;
+
+  const left = pathFinder(root.left,target);
+  const right = pathFinder(root.right,target);
+
+  return left  ? path.concat(left) : 
+         right ? path.concat(right) : null;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(pathFinder(a, 'e'),['a','b','e']);
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+module.exports = {
+  pathFinder,
+};
+/*
+
+*/
