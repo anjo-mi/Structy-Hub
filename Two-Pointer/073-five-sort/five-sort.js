9d28844a3717c2151bd4c3ef2ca4b9afa2ec4512
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-10 18:48:50 -0400

    Solved 'five sort' (node)

---
+++ Two-Pointer/073-five-sort/five-sort.js
@@ -0,0 +1,25 @@
+function fiveSort(nums){
+  let i = 0,
+      j = nums.length - 1;
+  while (i < j){
+    if (nums[i] !== 5) i++;
+    if (nums[j] === 5) j--;
+    if (nums[i] === 5 && nums[j] !== 5){
+      [nums[i],nums[j]] = [nums[j],nums[i]];
+      i++;
+      j--;
+    }
+  }
+  return nums;
+}
+
+console.log(fiveSort([5, 5, 6, 5, 5, 5, 5]),[6, 5, 5, 5, 5, 5, 5]);
+console.log(fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]),[4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]);
+console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]),[2, 2, 10, 6, 1, 5, 5, 5, 5, 5]);
+
+/*
+
+*/
+module.exports = {
+  fiveSort,
+};
