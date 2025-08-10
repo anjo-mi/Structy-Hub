55c6ae0e1b7df0e657c04ea16e4ba427bd443069
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-10 17:30:26 -0400

    Solved 'bottom right value' (node)

---
+++ Binary-Tree-I/062-bottom-right-value/bottom-right-value.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+const bottomRightValue = (root) => {
+  if (!root) return null;
+  const queue = [root];
+  let c = null;
+  while (queue.length){
+    const curr = queue.shift();
+    c = curr.val;
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+  return c;
+};
+/*
+
+*/
+const a = new Node(-1);
+const b = new Node(-6);
+const c = new Node(-5);
+const d = new Node(-3);
+const e = new Node(-4);
+const f = new Node(-13);
+const g = new Node(-2);
+const h = new Node(6);
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+e.right = h;
+console.log(bottomRightValue(a),6);
+//        -1
+//      /   \
+//    -6    -5
+//   /  \     \
+// -3   -4   -13
+//     / \       
+//    -2  6
+module.exports = {
+  bottomRightValue,
+};
