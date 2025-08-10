25226601fc838a9df111f9f3e0031ee7fc3b44f7
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-10 16:41:55 -0400

    Update get-node-value.js

--- Linked-List-I/039-get-node-value/get-node-value.js
+++ Linked-List-I/039-get-node-value/get-node-value.js
@@ -6,7 +6,9 @@ class Node {
 }
 
 const getNodeValue = (head, i) => {
-  
+  if (!head) return null;
+  if (!i) return head.val;
+  return getNodeValue(head.next, --i);
 };
 const node1 = new Node("banana");
 const node2 = new Node("mango");
