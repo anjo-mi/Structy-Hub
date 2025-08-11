9e59a535b5935212ed436fdb3f172112b5035815
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-11 11:24:38 -0400

    Solved 'has cycle' (node)

--- Graph-I/091-has-cycle/has-cycle.js
+++ Graph-I/091-has-cycle/has-cycle.js
@@ -1,46 +1,23 @@
 const hasCycle = (graph) => {
   for (const node in graph){
     const v = new Set();
-    const stack = [node];
-    while (stack.length){
-      const curr = stack.shift();
-      
+    const queue = [node];
+    while (queue.length){
+      const curr = queue.shift();
       v.add(curr);
       for (const n of graph[curr]){
         if (v.has(n)) return true;
-          stack.push(n);
+        queue.push(n);
       }
     }
   }
   return false;
 };
 /*
-for (const node in graph){
-    const v = new Set();
-    const stack = [node];
-    while (stack.length){
-      const curr = stack.shift();
-      v.add(curr);
-      for (const n of graph[curr]){
-        if (v.has(n)) return true;
-        stack.push(n);
-      }
-    }
-  }
-  return false;
-*/
-
-// after looping thru all nodes, if true hasnt been returned, return false
-/*
-p
-graph -> as an object  / adjacency list (array of strings props and values)
-r
-boolean, do any of the node in the graph form a cycle
-e
-no empty values in graph -> auto true but maybe not efficient
-p
 
 */
+
+
 console.log(hasCycle({
   a: ["b"],
   b: ["c"],
