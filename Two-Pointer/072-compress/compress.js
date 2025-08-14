4097e7db1867bff974465c8d22fcfd6bf415ddf7
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-14 13:21:12 -0400

    Solved 'compress' (node)

--- Two-Pointer/072-compress/compress.js
+++ Two-Pointer/072-compress/compress.js
@@ -1,14 +1,16 @@
 const compress = (s) => {
-  let str = '',
-      i = 0,
-      j = 0;
-  while (j < s.length){
-    while (s[i] === s[j]) j++;
-    const sub = s.slice(i,j);
-    str += sub.length > 1 ? sub.length + sub[0] : sub[0];
-    i = j;
+  let i = 0,
+      j = 0,
+      st = '';
+  while (i < s.length && j < s.length){
+    while (j < s.length && s[i] === s[j]) j++;
+    if (s[i] !== s[j]){
+      const sub = s.slice(i,j);
+      st += sub.length > 1 ? sub.length + sub[0] : sub[0];
+      i = j;
+    }
   }
-  return str;
+  return st;
 };
 console.log(compress('ccaaatsss'),'2c3at3s');
 /*
