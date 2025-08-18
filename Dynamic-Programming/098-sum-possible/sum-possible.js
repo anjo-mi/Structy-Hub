794194bcc9910456a0d1299bc79c803a6a2b1300
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-18 14:04:13 -0400

    Solved 'sum possible' (node)

---
+++ Dynamic-Programming/098-sum-possible/sum-possible.js
@@ -0,0 +1,49 @@
+const sumPossible = (amt, nums, memo = {}) => {
+  if (amt === 0) memo[amt] = true;
+  if (amt <   0) memo[amt] = false;
+  
+  if (amt in memo) return memo[amt];
+  
+  if (nums.some(num => sumPossible(amt-num,nums,memo))) memo[amt] = true;
+  else memo[amt] = false;
+  
+  return memo[amt];
+};
+/*
+add a memo = {} parameter
+if (amt === 0) memo[amt] = true;
+if (amt < 0) memo[amt] = false;
+if (amt in memo) return memo[amt];
+if sumPossible( (initial amount) - (any of the elements of number) ) memo[amt] = true;
+else memo[amt] = false
+
+return memo[amt]
+
+
+p
+Number + array of Numbers
+r
+Boolean, can any combo of nums in array sum up to the amt
+e
+negative values? (told initial Number will always be 0+),
+empty array? if number is not 0, return false, if number is 0 return true
+p
+add a memo = {} parameter
+if (amt === 0) memo[amt] = true;
+if (amt < 0) memo[amt] = false;
+
+if sumPossible( (initial amount) - (any of the elements of number) ) memo[amt] = true;
+else memo[amt] = false
+
+return memo[amt]
+
+*/
+console.log(sumPossible(8, [5, 12, 4]), true);
+console.log(sumPossible(15, [6, 2, 10, 19]), false);
+console.log(sumPossible(12, []), false);
+console.log(sumPossible(0, []), true);
+console.log(sumPossible(13, [3, 5]), true);
+
+module.exports = {
+  sumPossible,
+};
