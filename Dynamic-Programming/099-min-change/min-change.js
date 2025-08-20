38036f46b01d1b955459957d1801aaf187ddd5a4
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-20 11:42:06 -0400

    Solved 'min change' (node)

--- Dynamic-Programming/099-min-change/min-change.js
+++ Dynamic-Programming/099-min-change/min-change.js
@@ -1,40 +1,17 @@
-const minChange = (amt, coins, memo = {},depth = 0) => {
+const minChange = (amt, coins,memo={},depth = 0) => {
+  if (amt <   0) memo[amt] = Infinity;
   if (amt === 0) memo[amt] = 0;
-  if (amt < 0) return Infinity;
   if (amt in memo) return memo[amt];
-
   let min = Infinity;
   for (const coin of coins){
-    const cns = 1 + minChange(amt - coin, coins, memo, depth + 1);
-    if (cns < min) min = cns;
+    const change = minChange(amt-coin, coins, memo, depth + 1);
+    if (change + 1 < min) min = change + 1;
   }
-  
   memo[amt] = min;
-  return memo[amt] === Infinity && !depth ? -1 : memo[amt];
+  return memo[amt] === Infinity && depth === 0 ? -1 : memo[amt];
 };
 /*
-p
-Number (should always be positive?),
-Array of Numbers,
-prolly gona add a memo {} as a parameter
-r
-Number: least amount of combos needed from array to sum to Num parameter
-        or -1 if no combo from array sums to the number
-e
-negatives (seems impossible since you can't owe negative change)
-0 initial -> return 0
-empty array should be handled by the negative 1 return
 
-p : return memo[amt] at end!
-add memo to params
-base cases:
-- if (amt === 0) memo[amt] = 0
-- if (amt < 0) memo[amt] = -1 (maybe Infinity?)
-- if (amt in memo) return memo[amt]
-
-
-
-: return memo[amt] at end!
 */
 console.log(minChange(8, [1, 5, 4, 12]), 2);
 console.log(minChange(13, [1, 9, 5, 14, 30]), 5);
