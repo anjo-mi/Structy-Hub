f966f9d21fc8b5529b264c978597311205c54daf
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-20 11:27:36 -0400

    Solved 'fib' (node)

--- Dynamic-Programming/096-fib/fib.js
+++ Dynamic-Programming/096-fib/fib.js
@@ -1,9 +1,7 @@
 const fib = (n, memo ={}) => {
-  if (n in memo) return memo[n];
   if (n <= 1) memo[n] = n;
-  else{
-    memo[n] = fib(n-1, memo) + fib(n-2,memo);
-  }
+  if (n in memo) return memo[n];
+  memo[n] = fib(n-1,memo) + fib(n-2,memo);
   return memo[n];
 }
 
