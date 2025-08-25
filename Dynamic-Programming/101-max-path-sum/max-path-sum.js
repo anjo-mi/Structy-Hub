9c156d2613c9e5925ebc3aca5d39d4f39b4e2c5b
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-25 14:08:14 -0400

    Solved 'max path sum' (node)

---
+++ Dynamic-Programming/101-max-path-sum/max-path-sum.js
@@ -0,0 +1,83 @@
+const maxPathSum = (grid,r=0,c=0,memo={}) => {
+  if (!grid[r]?.[c]) return null;
+  const loc = r + ',' + c;
+  if (r === grid.length && c === grid[r].length) memo[loc] = grid[r][c];
+  if (loc in memo) return memo[loc];
+  memo[loc] = Math.max(
+    maxPathSum(grid,r+1,c,memo),
+    maxPathSum(grid,r,c+1,memo)
+  ) + grid[r][c];
+  return memo[loc];
+};
+/*
+p
+2d array of numbers in the form of grid
+will prolly add:
+  memo
+  row
+  column
+r
+number -> maximum total from top left to bottom right
+e
+negatives?
+uneven row lengths?
+p
+if (!grid[r]?.[c]) return null;
+const loc = r + ',' + c;
+if (r === grid.length && c = grid[r].length) memo[loc] = grid[r][c];
+if (loc in memo) return memo[loc];
+memo[loc] = Math.max(maxPathSum(grid,r+1,c,memo),maxPathSum(grid,r,c+1,memo)) + 1;
+return memo[loc];
+*/
+
+
+const grid = [
+  [1, 3, 12],
+  [5, 1, 1],
+  [3, 6, 1],
+];
+console.log(maxPathSum(grid), 18);
+const rid = [
+  [1, 1, 3, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 1, 1, 6, 1, 1, 5, 1, 1, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 5, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1, 8, 1, 1, 1, 1, 1, 1, 1],
+  [2, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 42, 1, 1, 1, 1, 1, 1, 1, 8, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+console.log(maxPathSum(rid), 82);
+const gri = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+console.log(maxPathSum(gri), 27);
+
+module.exports = {
+  maxPathSum,
+};
