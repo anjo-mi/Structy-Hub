3e3a49866de899bdaed6acfa30cd105171061e33
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-02 12:15:55 -0400

    Solved 'best bridge' (node)

--- Graph-I/090-best-bridge/best-bridge.js
+++ Graph-I/090-best-bridge/best-bridge.js
@@ -2,52 +2,56 @@ const loc = (r,c) => r + ',' + c;
 const coords = (l) => l.split(',').map(Number);
 const inBounds = (r,c,grid) => grid[r]?.[c];
 
-
-const getConnections = (r,c,grid,isle = new Set())=>{
+const getConnections = (r,c,grid,isle = new Set()) => {
   if (!inBounds(r,c,grid) ||
       grid[r][c] !== 'L' ||
       isle.has(loc(r,c))) return isle;
   isle.add(loc(r,c));
-  getConnections(r+1,c,grid,isle);
+
   getConnections(r-1,c,grid,isle);
-  getConnections(r,c+1,grid,isle);
+  getConnections(r+1,c,grid,isle);
   getConnections(r,c-1,grid,isle);
+  getConnections(r,c+1,grid,isle);
+
   return isle;
 }
 
-const findBridge = (l,grid,b,bridge = new Set()) => {
-  const q = [{l,dist:-1}];
+const findBridge = (l, grid, b, checked = new Set()) => {
+  const q = [{l, dist: -1}];
   while (q.length){
-    const {l, dist} = q.shift();
-    if (b.has(l)) return dist;
+    const {l,dist} = q.shift();
     const [r,c] = coords(l);
-    if (bridge.has(l) || !inBounds(r,c,grid)) continue;
-    bridge.add(l);
-    if (inBounds(r+1,c,grid)) q.push({l: loc(r+1,c), dist: dist + 1});
-    if (inBounds(r-1,c,grid)) q.push({l: loc(r-1,c), dist: dist + 1});
-    if (inBounds(r,c+1,grid)) q.push({l: loc(r,c+1), dist: dist + 1});
-    if (inBounds(r,c-1,grid)) q.push({l: loc(r,c-1), dist: dist + 1});
+    if (!inBounds(r,c,grid) || checked.has(l)) continue;
+    checked.add(l);
+    if (b.has(l)) return dist;
+    q.push({l: loc(r+1,c), dist: dist + 1});
+    q.push({l: loc(r-1,c), dist: dist + 1});
+    q.push({l: loc(r,c-1), dist: dist + 1});
+    q.push({l: loc(r,c+1), dist: dist + 1});
+    
   }
-  return -1;
+  return Infinity;
 }
 
 const bestBridge = (grid) => {
   const islands = [];
   for (let r = 0 ; r < grid.length ; r++){
-    for (let c = 0 ; c < grid[r].length ; c++){
+    for (let c = 0 ; c < grid.length ; c++){
       if (grid[r][c] === 'L' && !islands.some(isle => isle.has(loc(r,c)))){
         const isle = getConnections(r,c,grid);
         islands.push(isle);
       }
     }
   }
+
   const [a,b] = islands;
-  let smallestBridge = Infinity;
+  console.log({islands,a,b})
+  let best = Infinity;
   for (const l of a){
-    const bridgeDist = findBridge(l,grid,b);
-    if (bridgeDist < smallestBridge) smallestBridge = bridgeDist;
+    const br = findBridge(l, grid, b);
+    if (br < best) best = br;
   }
-  return smallestBridge;
+  return best;
 };
 
 /*
