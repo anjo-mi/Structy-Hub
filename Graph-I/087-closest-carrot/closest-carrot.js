980dbc20488e8626a256377a4f36f149bacbcd0d
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-03 11:29:06 -0400

    Solved 'closest carrot' (node)

--- Graph-I/087-closest-carrot/closest-carrot.js
+++ Graph-I/087-closest-carrot/closest-carrot.js
@@ -3,18 +3,20 @@ const coords = (l) => l.split(',').map(Number);
 const inBounds = (r,c,grid) => grid[r]?.[c];
 
 const closestCarrot = (grid, sr, sc) => {
-  const q = [{l:loc(sr,sc), dist:0}];
+  const q = [{l:loc(sr,sc), dist: 0}];
   const v = new Set();
   while (q.length){
     const {l,dist} = q.shift();
     const [r,c] = coords(l);
-    if (v.has(l) || !inBounds(r,c,grid) || grid[r][c] === 'X') continue;
+    if (!inBounds(r,c,grid) ||
+        grid[r][c] === 'X' ||
+        v.has(l)) continue;
     v.add(l);
     if (grid[r][c] === 'C') return dist;
-    q.push({l:loc(r+1,c), dist: dist + 1});
     q.push({l:loc(r-1,c), dist: dist + 1});
-    q.push({l:loc(r,c+1), dist: dist + 1});
+    q.push({l:loc(r+1,c), dist: dist + 1});
     q.push({l:loc(r,c-1), dist: dist + 1});
+    q.push({l:loc(r,c+1), dist: dist + 1});
   }
   return -1;
 };
