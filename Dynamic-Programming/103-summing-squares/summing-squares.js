73607bfc87434a607179d49c3ecd1b3f73ccaa95
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-05 11:47:50 -0400

    Solved 'summing squares' (node)

--- Dynamic-Programming/103-summing-squares/summing-squares.js
+++ Dynamic-Programming/103-summing-squares/summing-squares.js
@@ -1,40 +1,14 @@
-const summingSquares = (n, memo={}) => {
-  if (n === 0) memo[n] = 0;
-  if (n <   0) memo[n] = Infinity;
+const summingSquares = (n, memo = {}) => {
   if (n in memo) return memo[n];
-  const squares = [];
-  for (let i = 1 ; i * i <= n ; i++){
-    squares.push(i*i);
-  }
-  // let min = Infinity;
-  const results = squares.map(sq => summingSquares(n - sq, memo) + 1);
-  memo[n] = Math.min(...results);
+  if (n === 0) return 0;
+  const sqs = [];
+  for (let i = 1 ; i * i <= n ; i++) sqs.push(i*i);
+  const counts = sqs.map(sq => summingSquares(n-sq,memo) + 1);
+  memo[n] = Math.min(...counts);
   return memo[n];
 };
+
 /*
-n = 0 memo[n] = 0
-if n in memo return memo[n]
-let min = Infinity
-loop thru array of squares up to n
-subtract each element from n and run summingSquares on it
-the minimum value of that (if existent) = min
-memo[n] = min
-return memo[n]
-p
-Number, (add memo obj)
-r
-Number minimum times any perfect square can add to total up to number (1, 4, 9, 16,...)
-e
-negative numbers (return Infinity?)
-p
-n = 0 memo[n] = 0
-if n in memo return memo[n]
-let min = Infinity
-loop thru array of squares up to n
-subtract each element from n and run summingSquares on it
-the minimum value of that (if existent) = min
-memo[n] = min
-return memo[n]
 
 */
 
