c7e1307d9d7f8d87d03173daabee6aa10f331b1d
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-07 13:26:48 -0400

    Solved 'counting change' (node)

--- Dynamic-Programming/104-counting-change/counting-change.js
+++ Dynamic-Programming/104-counting-change/counting-change.js
@@ -1,38 +1,40 @@
 const countingChange = (amt, coins, i = 0, memo = {}) => {
-  let k = amt + ',' + i;
-  if (k in memo) return memo[k];
-  if (amt < 0) return 0;
   if (amt === 0) return 1;
-  if (i >= coins.length) return 0;
-  const coin = coins[i];
+  if (amt <   0) return 0;
+  const k = amt + ';' + i;
+  if (k in memo) return memo[k];
   let result = 0;
-  for (let numOfCoins = 0 ; numOfCoins * coin <= amt ; numOfCoins++){
-    const total = numOfCoins * coin;
-    result += countingChange(amt - total, coins, i + 1, memo);
+  const coin = coins[i];
+  for (let q = 0 ; q * coin <= amt ; q++){
+    result += countingChange(amt - (q * coin), coins, i + 1, memo);
   }
   memo[k] = result;
   return memo[k];
 };
 console.log(countingChange(24, [3, 5, 7]), 5);
-// console.log(countingChange(13, [2, 6, 12, 10]), 0);
-// console.log(countingChange(240, [1, 2, 3, 4, 5, 6, 7, 8, 9]), 1525987916);
+console.log(countingChange(13, [2, 6, 12, 10]), 0);
+console.log(countingChange(240, [1, 2, 3, 4, 5, 6, 7, 8, 9]), 1525987916);
 /*
-set up the parameters to:
-  track the position of coins that were on
-  make a key pertaining to the amount and the position of coin we're at
+add a parameter for the index to keep track of position in coins array
+add a parameter for memoizing
 
-if (amount < 0) return 0
-if (amount === 0) return 1
-if (key in memo) return memo[key]
+if the amount is = 0, (success), return 1
+if the amount is < 0, (failure), return 0
+
+make a key combining the amount remaining at this position in the coin array, + the position in the coin array
+
+if that key exists in memo, return memo[key]
+
 let result = 0;
-for each denomination of the coin up until it is greater than the amount
-  call the function
-  if the function eventually equates to 0
-    add none to result
-  if the function eventually equates to 1
-    add one to the result
-memo[key] = result
-return it
+
+for every quantity of the current coin we can deduct from the amount
+  if a recursive call on that new amount is success, add 1 to the current result
+  if a recursive call on that new amount is failure, add 0 to the current result
+    (both are handled by the return at top of function)
+
+memo for the current amount at the current level = the number of successes we just encountered at this current amount and current level
+
+return that result
 p
 Number, array of Numbers, (added memo object)
 r
