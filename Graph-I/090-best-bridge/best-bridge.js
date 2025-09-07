1eb0f14bfc4e7a4e61b5f55eba16021f3b63e493
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-06 21:15:09 -0400

    Solved 'best bridge' (node)

--- Graph-I/090-best-bridge/best-bridge.js
+++ Graph-I/090-best-bridge/best-bridge.js
@@ -4,8 +4,8 @@ const inBounds = (r,c,grid) => grid[r]?.[c];
 
 const getConnections = (r,c,grid,isle = new Set()) => {
   if (!inBounds(r,c,grid) ||
-      grid[r][c] !== 'L' ||
-      isle.has(loc(r,c))) return isle;
+       grid[r][c] !== 'L' || 
+       isle.has(loc(r,c))) return isle;
   isle.add(loc(r,c));
 
   getConnections(r-1,c,grid,isle);
@@ -16,19 +16,18 @@ const getConnections = (r,c,grid,isle = new Set()) => {
   return isle;
 }
 
-const findBridge = (l, grid, b, checked = new Set()) => {
-  const q = [{l, dist: -1}];
+const findBridge = (l,grid,b,br = new Set()) =>{
+  const q = [{l,dist: -1}];
   while (q.length){
     const {l,dist} = q.shift();
     const [r,c] = coords(l);
-    if (!inBounds(r,c,grid) || checked.has(l)) continue;
-    checked.add(l);
+    if (!inBounds(r,c,grid) || br.has(l)) continue;
+    br.add(l);
     if (b.has(l)) return dist;
-    q.push({l: loc(r+1,c), dist: dist + 1});
-    q.push({l: loc(r-1,c), dist: dist + 1});
-    q.push({l: loc(r,c-1), dist: dist + 1});
-    q.push({l: loc(r,c+1), dist: dist + 1});
-    
+    q.push({l: loc(r-1,c), dist: dist + 1})
+    q.push({l: loc(r+1,c), dist: dist + 1})
+    q.push({l: loc(r,c-1), dist: dist + 1})
+    q.push({l: loc(r,c+1), dist: dist + 1})
   }
   return Infinity;
 }
@@ -36,19 +35,17 @@ const findBridge = (l, grid, b, checked = new Set()) => {
 const bestBridge = (grid) => {
   const islands = [];
   for (let r = 0 ; r < grid.length ; r++){
-    for (let c = 0 ; c < grid.length ; c++){
+    for (let c = 0 ; c < grid[r].length ; c++){
       if (grid[r][c] === 'L' && !islands.some(isle => isle.has(loc(r,c)))){
         const isle = getConnections(r,c,grid);
         islands.push(isle);
       }
     }
   }
-
   const [a,b] = islands;
-  console.log({islands,a,b})
   let best = Infinity;
   for (const l of a){
-    const br = findBridge(l, grid, b);
+    const br = findBridge(l,grid,b);
     if (br < best) best = br;
   }
   return best;
