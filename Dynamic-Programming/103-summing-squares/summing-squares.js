da002338a12a13371d5191f8f3a54d9caf8f6dfa
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-07 14:08:30 -0400

    Solved 'summing squares' (node)

--- Dynamic-Programming/103-summing-squares/summing-squares.js
+++ Dynamic-Programming/103-summing-squares/summing-squares.js
@@ -2,16 +2,18 @@ const summingSquares = (n, memo = {}) => {
   if (n in memo) return memo[n];
   if (n === 0) return 0;
   const sqs = [];
-  for (let i = 1 ; i * i <= n ; i++) sqs.push(i*i);
-  const counts = sqs.map(sq => summingSquares(n-sq,memo) + 1);
-  memo[n] = Math.min(...counts);
+  for (let i = 1 ; i * i <= n ; i++){
+    sqs.push(i*i);
+  }
+  const poss = sqs.map(sq => summingSquares(n-sq,memo) + 1);
+  memo[n] = Math.min(...poss);
   return memo[n];
 };
 
 /*
 
 */
-
+console.log(summingSquares(12), 3);
 console.log(summingSquares(8), 2);
 console.log(summingSquares(31), 4);
 console.log(summingSquares(87), 4);
