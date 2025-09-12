3bb7f21bbe154db46407fa63748ccb04052416f7
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-12 10:54:03 -0400

    Solved 'reverse list' (node)

---
+++ Linked-List-I/042-reverse-list/reverse-list.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const reverseList = (head) => {
+  if (!head) return null;
+  let curr = head,
+      prev = null;
+  while (curr){
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
+/*
+
+*/
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+console.log(reverseList(a));
+const x = new Node("x");
+const y = new Node("y");
+x.next = y;
+console.log(reverseList(x));
+const p = new Node("p");
+console.log(reverseList(p));
+module.exports = {
+  reverseList,
+};
