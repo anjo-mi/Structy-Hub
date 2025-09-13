c9347f53b3fe0f8640e19761d75132dcc5a36983
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-13 12:33:25 -0400

    Solved 'level averages' (node)

---
+++ Binary-Tree-I/070-level-averages/level-averages.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const levelAverages = (root) => {
+  const avgs = [];
+  if (!root) return avgs;
+  let q = [root];
+  while (q.length){
+    const avg = q.reduce((a,el) => a + el.val, 0) / q.length;
+    avgs.push(avg);
+    const next = [];
+    for (const n of q){
+      if (n.left) next.push(n.left);
+      if (n.right) next.push(n.right);
+    }
+    q = next;
+  }
+  return avgs;
+};
+/*
+
+*/
+const a = new Node(5);
+const b = new Node(11);
+const c = new Node(54);
+const d = new Node(20);
+const e = new Node(15);
+const f = new Node(1);
+const g = new Node(3);
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+e.left = f;
+e.right = g;
+console.log(levelAverages(a), [ 5, 32.5, 17.5, 2 ] );
+//        5
+//     /    \
+//    11    54
+//  /   \
+// 20   15
+//      / \
+//     1  3
+
+
+module.exports = {
+  levelAverages,
+};
