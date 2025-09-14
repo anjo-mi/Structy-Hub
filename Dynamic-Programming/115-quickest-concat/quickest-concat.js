8f85844120b3c129ea3e7f1dd184e6485b66a8ac
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-14 12:29:47 -0400

    Solved 'quickest concat' (node)

--- Dynamic-Programming/115-quickest-concat/quickest-concat.js
+++ Dynamic-Programming/115-quickest-concat/quickest-concat.js
@@ -3,60 +3,24 @@ const quickestConcat = (s, words, depth = 0, i = 0, memo = {}) => {
   if (i >   s.length) return Infinity;
   if (i in memo) return memo[i];
 
-  const remainingString = s.slice(i);
+  const remnant = s.slice(i);
   let min = Infinity;
-
   for (const word of words){
-    if (remainingString.startsWith(word)){
-      const result = quickestConcat(s, words,depth + 1, i + word.length, memo) + 1;
-      if (result < min) min = result;
+    if (remnant.startsWith(word)){
+      poss = quickestConcat(s, words, depth + 1, i + word.length, memo) + 1;
+      if (poss < min) min = poss;
     }
   }
-
   memo[i] = min;
-  return !isFinite(memo[i]) && !depth ? -1 : memo[i];
+  return !isFinite(min) && !depth ? -1 : memo[i];
 };
+
 console.log(quickestConcat('simchacindy', ['sim', 'simcha', 'acindy']), -1);
 console.log(quickestConcat('caution', ['ca', 'ion', 'caut', 'ut']), 2);
 console.log(quickestConcat('respondorreact', ['re', 'or', 'spond', 'act', 'respond']), 4);
 console.log(quickestConcat('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', ['u', 'uu', 'uuu', 'uuuu', 'uuuuu']), 7);
 console.log(quickestConcat('rongbetty', ['wrong', 'bet']), -1);
 
-// add parameters:
-//   i = 0
-//   memo = {}
-// if (i === s.length) return 0
-// if (i >   s.length) return Infinity
-// if (i in memo) return memo[i];
-
-// slice the string at index (i)
-// make a min variable set to Infinity
-
-// for every word in the array=>
-//   if the sliced string starts with the word
-//     call the function again
-//       with ORIGINAL WORD (not sliced word)
-//       index + word.length
-//       (and add 1?) to result
-//     if result is less than min,, min = result
-
-// memo[i] = min
-// return memo[i]
-/*
-p
-String, String Array
-prolly add:
-  index tracker
-  memo object
-r
-Number, fewwest amount of elements that can be concatenated to make the String
-e
-same result from multiple elements (return either)
-empty string? return 0?
-empty array - return -1
-p
-
-*/
 module.exports = {
   quickestConcat,
 };
