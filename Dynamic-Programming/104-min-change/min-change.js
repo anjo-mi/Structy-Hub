725b2b35a3979b16e4aae85a10694f85bb2ec1bd
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-15 10:52:51 -0400

    Solved 'min change' (node)

---
+++ Dynamic-Programming/104-min-change/min-change.js
@@ -0,0 +1,20 @@
+const minChange = (amt,coins,memo={},depth = 0) => {
+  if (!amt) return 0;
+  if (amt < 0) return Infinity;
+  if (amt in memo) return memo[amt];
+
+  const poss = coins.map(cn => minChange(amt-cn,coins,memo,depth+1) + 1);
+  memo[amt] = Math.min(...poss);
+  return !isFinite(memo[amt]) && !depth ? -1 : memo[amt];
+};
+/*
+
+*/
+console.log(minChange(8, [1, 5, 4, 12]), 2);
+console.log(minChange(13, [1, 9, 5, 14, 30]), 5);
+console.log(minChange(102, [1, 5, 10, 25]) ,6);
+console.log(minChange(271, [10, 8, 265, 24]), -1);
+
+module.exports = {
+  minChange,
+};
