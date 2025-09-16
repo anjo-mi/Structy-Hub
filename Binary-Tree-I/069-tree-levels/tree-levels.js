dc7ced3c2655a4204e873b8a2195023863037fc4
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-16 11:01:24 -0400

    Solved 'tree levels' (node)

---
+++ Binary-Tree-I/069-tree-levels/tree-levels.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const treeLevels = (root) => {
+  const levels = [];
+  if (!root) return levels;
+  let q = [root];
+  while (q.length){
+    const level = q.map(n => n.val);
+    levels.push(level);
+    const next = [];
+    for (const n of q){
+      if (n.left) next.push(n.left);
+      if (n.right) next.push(n.right);
+    }
+    q = next;
+  }
+  return levels;
+};
+/*
+
+*/
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+const i = new Node("i");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+e.right = h;
+f.left = i;
+console.log(treeLevels(a),
+[
+  ['a'],
+  ['b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i']
+])
+//         a
+//      /    \
+//     b      c
+//   /  \      \
+//  d    e      f
+//      / \    /
+//     g  h   i
+
+
+
+module.exports = {
+  treeLevels,
+};
