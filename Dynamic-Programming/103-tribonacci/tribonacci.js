bdab73b241ccc3e231977524ef8c6779a0cfa011
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-16 11:03:56 -0400

    Solved 'tribonacci' (node)

---
+++ Dynamic-Programming/103-tribonacci/tribonacci.js
@@ -0,0 +1,19 @@
+const tribonacci = (n,memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 1) return 0;
+  if (n === 2) return 1;
+
+  memo[n] = tribonacci(n-1,memo) + tribonacci(n-2,memo) + tribonacci(n-3,memo);
+  return memo[n];
+}
+
+/*
+
+*/
+
+console.log(tribonacci(37), 1132436852);
+console.log(tribonacci(7), 13);
+console.log(tribonacci(1), 0);
+module.exports = {
+  tribonacci,
+};
