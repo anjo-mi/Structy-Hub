5a532a1ab963fbf14bfd3a953d0d9e2805f514b9
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-17 11:18:01 -0400

    Solved 'uncompress' (node)

---
+++ Two-Pointer/076-uncompress/uncompress.js
@@ -0,0 +1,21 @@
+const uncompress = (s) => {
+  const arab = new Set('0123456789');
+  let i = 0,
+      j = 0,
+      st = '';
+  while (j < s.length){
+    while (arab.has(s[j])) j++;
+    const num = +s.slice(i,j);
+    st += s[j].repeat(num);
+    j++;
+    i = j;
+  }
+  return st;
+};
+
+console.log(uncompress("2c3a1t"),'ccaaat');
+console.log(uncompress("3n12e2z"),'nnneeeeeeeeeeeezz');
+
+module.exports = {
+  uncompress,
+};
