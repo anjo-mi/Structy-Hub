4eabdebdd048aac52e58c6664e5fba043e2e3fc5
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-18 11:35:44 -0400

    Solved 'counting change' (node)

---
+++ Dynamic-Programming/110-counting-change/counting-change.js
@@ -0,0 +1,23 @@
+const countingChange = (amt, coins, i = 0, memo = {}) => {
+  const k = amt + ';' + i;
+  if (k in memo) return memo[k];
+  if (amt <   0) return 0;
+  if (amt === 0) return 1;
+
+  let result = 0;
+  const coin = coins[i];
+  for (let count = 0 ; count * coin <= amt ; count++){
+    result += countingChange(amt - (count * coin), coins, i + 1, memo);
+  }
+  memo[k] = result;
+  return memo[k];
+};
+console.log(countingChange(24, [3, 5, 7]), 5);
+console.log(countingChange(13, [2, 6, 12, 10]), 0);
+console.log(countingChange(240, [1, 2, 3, 4, 5, 6, 7, 8, 9]), 1525987916);
+/*
+
+*/
+module.exports = {
+  countingChange,
+};
