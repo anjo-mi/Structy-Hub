db5178df1f8e720368bdb83c36496e9d46fd0338
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-19 10:10:04 -0400

    Solved 'has path' (node)

---
+++ Graph-I/083-has-path/has-path.js
@@ -0,0 +1,35 @@
+const hasPath = (graph, src, dst) => {
+  const v =  new Set();
+  const q = [src];
+  while (q.length){
+    const curr = q.shift();
+    if (v.has(curr)) continue;
+    v.add(curr);
+    if (curr === dst) return true;
+    for (const n of graph[curr]) q.push(n);
+  }
+  return false;
+};
+/*
+
+*/
+const graph = {
+  f: ['g', 'i'],
+  g: ['h'],
+  h: [],
+  i: ['g', 'k'],
+  j: ['i'],
+  k: []
+};
+console.log(hasPath(graph, 'f', 'j'),false);
+const grap = {
+  v: ['x', 'w'],
+  w: [],
+  x: [],
+  y: ['z'],
+  z: [],  
+};
+console.log(hasPath(grap, 'v', 'w'), true);
+module.exports = {
+  hasPath,
+};
