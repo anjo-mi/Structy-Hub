c012239821e878157e3fe575e9481a5a1f11b364
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-20 12:26:58 -0400

    Solved 'can concat' (node)

--- Dynamic-Programming/115-can-concat/can-concat.js
+++ Dynamic-Programming/115-can-concat/can-concat.js
@@ -1,11 +1,9 @@
 const canConcat = (s, words, i = 0, memo = {}) => {
   if (i === s.length) return true;
   if (i >   s.length) return false;
-  if (i in memo) return memo[i];
+  if (i in  memo    ) return memo[i];
 
-  const rem = s.slice(i);
-  memo[i] = words.some(w => rem.startsWith(w) && canConcat(s,words,i+w.length,memo));
-  
+  memo[i] = words.some(w => s.startsWith(w,i) && canConcat(s,words,i+w.length,memo));
   return memo[i];
 };
 console.log(canConcat("oneisnone", ["one", "none", "is"]), true);
