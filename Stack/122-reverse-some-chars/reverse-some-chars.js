22692ec0ac71576f6b0f4a022136d518259fb58d
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-21 12:53:40 -0400

    Solved 'reverse some chars' (node)

--- Stack/122-reverse-some-chars/reverse-some-chars.js
+++ Stack/122-reverse-some-chars/reverse-some-chars.js
@@ -1,38 +1,15 @@
 const reverseSomeChars = (str, chars) => {
   chars = new Set(chars);
-  const stack = [];
-  for (const c of str) if (chars.has(c)) stack.push(c);
-  let replacement = '';
-  for (const c of str){
-    if(chars.has(c)) replacement += stack.pop();
-    else replacement += c;
+  const remmed = [];
+  for (const char of str){
+    if (chars.has(char)) remmed.push(char);
   }
-  return replacement;
+  return str.split('').map(char => {
+    if (chars.has(char)) return remmed.pop();
+    return char;
+  }).join('');
 };
-/*
-OPTIMIZE: dont map
-  just iterate thru and add matches to stack
-  second time you iterate, replace matches with stack.pop()
-  return orig?
 
-make a set from chars
-make an empty stack = [];
-map thru split string, any char that is in the set, gets replaced by something that we can retrieve
-while (the stack has a length);
-  replace retrievable with stack.pop()
-
-return mapped split string join('')
-p
-str: String
-chars: [String (single letters in these examples)];
-r
-String
-  -> characters in chars array that appear in String
-      should be in reverse order
-e
-chars[element].length > 1; (can still use well)
-p
-*/
 
 console.log(reverseSomeChars("computer", ["a", "e", "i", "o", "u"]), 'cemputor');
 console.log(reverseSomeChars("skateboard", ["a", "e", "i", "o", "u"]), 'skatobeard');
