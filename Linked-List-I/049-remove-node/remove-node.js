a3fc0a73ad554cb415995ea9743667b08b4faf9e
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-21 12:49:55 -0400

    Solved 'remove node' (node)

--- Linked-List-I/049-remove-node/remove-node.js
+++ Linked-List-I/049-remove-node/remove-node.js
@@ -6,10 +6,11 @@ class Node {
 }
 
 const removeNode = (head, target) => {
-  if (!head) return head;
+  if (!head) return null;
   if (head.val === target) return head.next;
-  let prev = null,
-      curr = head;
+
+  let curr = head,
+      prev = null;
   while (curr){
     if (curr.val === target){
       prev.next = curr.next;
