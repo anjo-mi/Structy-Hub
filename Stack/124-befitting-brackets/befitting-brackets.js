6b974d2647677a005ae7f43e50e7195bfcea76e3
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-22 11:47:48 -0400

    Solved 'befitting brackets' (node)

--- Stack/124-befitting-brackets/befitting-brackets.js
+++ Stack/124-befitting-brackets/befitting-brackets.js
@@ -1,46 +1,31 @@
 const befittingBrackets = (str) => {
-  const stack = [];
-  const pushes = new Set('({[');
-  const pulls  = new Set(')}]');
-  const correspond = {
+  const closes = {
     '}' : '{',
     ']' : '[',
-    ')' : '('
+    ')' : '(',
   }
-
+  const opens = {
+    '{' : '}',
+    '[' : ']',
+    '(' : ')',
+  }
+  const stack = [];
   for (const char of str){
-    if (pushes.has(char)) stack.push(char);
-    if (pulls.has(char)){
-      if (!stack.includes(correspond[char])) return false;
+    if (char in opens) stack.push(char);
+    if (char in closes){
+      if (stack[stack.length - 1] !== closes[char]) return false;
       stack.pop();
     }
   }
   return !stack.length;
 };
+
+
 console.log(befittingBrackets('(){}[](())'), true);
 console.log(befittingBrackets('[][}'), false);
 console.log(befittingBrackets('{[]}({}'), false);
 console.log(befittingBrackets(''), true);
-/*
-create a stack variable: []
 
-if character is {,(,or [ push to stack
-if character is },),or ]
-  if open is in stack: remove corresponding opening bracket from stack (pop)
-  else return false
-
-return !stack.length
-p
-String (containing ONLY: '[]{}()')
-r
-Boolean: is the combo in proper order
-  no matching close bracket before its open bracket
-  no mathcing open bracket after its close bracket
-e
-empty string (return true)
-non bracket characters? (filter out?)
-p
-*/
 
 
 
