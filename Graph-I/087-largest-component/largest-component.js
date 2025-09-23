ae964b2bcfada86a9df0ee25741e2afc91858544
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-23 11:07:43 -0400

    Solved 'largest component' (node)

--- Graph-I/087-largest-component/largest-component.js
+++ Graph-I/087-largest-component/largest-component.js
@@ -12,17 +12,17 @@ const largestComponent = (graph) => {
     }
     islands.push(isle);
   }
-  const max = Math.max(...islands.map(isle => isle.size));
-  return isFinite(max) ? max : 0;
+  return islands.length ? Math.max(...islands.map(isle => isle.size)) : 0;
 };
-// console.log(largestComponent({
-//   1: ['2'],
-//   2: ['1','8'],
-//   6: ['7'],
-//   9: ['8'],
-//   7: ['6', '8'],
-//   8: ['9', '7', '2']
-// }), 6);
+console.log(largestComponent({
+  1: ['2'],
+  2: ['1','8'],
+  6: ['7'],
+  9: ['8'],
+  7: ['6', '8'],
+  8: ['9', '7', '2']
+}), 6);
+console.log(largestComponent({}), 0);
 console.log(largestComponent({
   0: ['8', '1', '5'],
   1: ['0'],
