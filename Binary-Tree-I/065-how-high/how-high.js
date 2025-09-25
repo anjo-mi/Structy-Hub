6e3bb40f1a81c828b6247a9c8e1646d1df064432
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-25 10:09:46 -0400

    Solved 'how high' (node)

--- Binary-Tree-I/065-how-high/how-high.js
+++ Binary-Tree-I/065-how-high/how-high.js
@@ -9,8 +9,10 @@ class Node {
 const howHigh = (root,depth = 0) => {
   if (!root) return -1;
   if (!root.right && !root.left) return depth;
-
-  return Math.max(howHigh(root.left, depth + 1), howHigh(root.right, depth + 1));
+  return Math.max(
+    howHigh(root.left,depth+1),
+    howHigh(root.right,depth+1)
+  )
 };
 /*
 */
