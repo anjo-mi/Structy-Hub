7ecbe0f85523549dedbf9992b16f816cac17c161
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-25 09:52:12 -0400

    Solved 'can concat' (node)

--- Dynamic-Programming/115-can-concat/can-concat.js
+++ Dynamic-Programming/115-can-concat/can-concat.js
@@ -1,7 +1,7 @@
 const canConcat = (s, words, i = 0, memo = {}) => {
   if (i === s.length) return true;
-  if (i >   s.length) return false;
-  if (i in  memo    ) return memo[i];
+  if (i  >  s.length) return false;
+  if (i in memo) return memo[i];
 
   memo[i] = words.some(w => s.startsWith(w,i) && canConcat(s,words,i+w.length,memo));
   return memo[i];
