7048858cb00474c957ba58ada921241dd4b7433c
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-26 09:40:19 -0400

    Solved 'array stepper' (node)

--- Dynamic-Programming/111-array-stepper/array-stepper.js
+++ Dynamic-Programming/111-array-stepper/array-stepper.js
@@ -1,9 +1,13 @@
-const arrayStepper = (nums) => {
-  let goal = nums.length - 1;
-  for (let i = nums.length - 2 ; i >= 0 ; i--){
-    if (nums[i] + i >= goal) goal = i;
-  }
-  return !goal;
+const arrayStepper = (nums,i=0,memo={}) => {
+  if (i === nums.length - 1) return true;
+  if (i in memo) return memo[i];
+
+  const steps = [];
+  for (let step = 1 ; step <= nums[i] ; step++) steps.push(step);
+
+  memo[i] = steps.some(step => arrayStepper(nums, i + step, memo));
+
+  return memo[i];
 };
 console.log(arrayStepper([2, 4, 2, 0, 0, 1]), true);
 console.log(arrayStepper([4, 1, 2, 1, 1, 1, 0, 4]), false);
