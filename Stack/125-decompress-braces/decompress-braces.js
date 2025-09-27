01dc2cda34b58990b85a638d5804c9094f07da74
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-27 11:04:46 -0400

    Solved 'decompress braces' (node)

--- Stack/125-decompress-braces/decompress-braces.js
+++ Stack/125-decompress-braces/decompress-braces.js
@@ -1,28 +1,24 @@
 const decompressBraces = (s) => {
   const arab = new Set('123456789');
-  const stops = new Set('{}');
   const stack = [];
   for (let i = 0 ; i < s.length ; i++){
-    if (!s.includes('{')) break;
     if (s[i] === '{') stack.push(i);
-
     if (s[i] === '}'){
-      const start = stack.pop() + 1;
-      let ind = start - 2;
-      while (arab.has(s[ind])) ind--;
-      
-      const num = +s.slice(ind + 1,start - 1);
-      const section = s.slice(start,i);
-      const replace = s.slice(ind + 1,i + 1);
-      s = s.replace(replace, section.repeat(num));
-      i = s.lastIndexOf(section) + section.length - 1;
+      const start = stack.pop();
+      const gettingReplaced = s.slice(start, i+1);
+      const exp = gettingReplaced.slice(1,-1);
+      let numInd = start - 1;
+      while (arab.has(s[numInd-1])) numInd--;
+      const num = +s.slice(numInd,start)
+      s = s.replace(s.slice(numInd, i+1),exp.repeat(num));
+      i = s.lastIndexOf(exp + exp.length);
     }
   }
   return s;
 };
-// console.log(decompressBraces("2{q}3{tu}v"), 'qqtututuv');
-// console.log(decompressBraces("2{y3{o}}s"), 'yoooyooos');
-// console.log(decompressBraces("2{3{r4{e}r}io}"), 'reeeerreeeerreeeerioreeeerreeeerreeeerio');
+console.log(decompressBraces("2{q}3{tu}v"), 'qqtututuv');
+console.log(decompressBraces("2{y3{o}}s"), 'yoooyooos');
+console.log(decompressBraces("2{3{r4{e}r}io}"), 'reeeerreeeerreeeerioreeeerreeeerreeeerio');
 /*
 
 */
