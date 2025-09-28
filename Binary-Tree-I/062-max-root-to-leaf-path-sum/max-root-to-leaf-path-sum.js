93a3f13ef9bd1e82c7509d6e74419ee436e0435b
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-28 10:13:53 -0400

    Solved 'max root to leaf path sum' (node)

--- Binary-Tree-I/062-max-root-to-leaf-path-sum/max-root-to-leaf-path-sum.js
+++ Binary-Tree-I/062-max-root-to-leaf-path-sum/max-root-to-leaf-path-sum.js
@@ -9,7 +9,7 @@ class Node {
 const maxPathSum = (root) => {
   if (!root) return null;
   const total = root.val;
-  if (!root.right && !root.left) return total;
+  if (!root.left && !root.right) return total;
 
   const left = maxPathSum(root.left);
   const right = maxPathSum(root.right);
