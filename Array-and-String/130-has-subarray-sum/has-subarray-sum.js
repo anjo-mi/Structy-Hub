3252ff24a469df7fef8b76bb81d17116f990d47c
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-29 10:03:18 -0400

    Solved 'has subarray sum' (node)

--- Array-and-String/130-has-subarray-sum/has-subarray-sum.js
+++ Array-and-String/130-has-subarray-sum/has-subarray-sum.js
@@ -1,28 +1,11 @@
-const hasSubarraySum = (nums, sum, i = 0, j = nums.length, memo = {}) => {
-  const k = i + ';' + j;
-  if (i === j) return nums[i] === sum;
-  if (i  >  j) return false;
-  if (k in memo) return memo[k];
-  if (nums.slice(i,j).reduce((a,el) => a + el, 0) === sum) memo[k] = true;
-  else memo[k] = hasSubarraySum(nums,sum,i+1,j) || hasSubarraySum(nums,sum,i,j-1);
-  return memo[k];
+const hasSubarraySum = (nums, sum, i = 0 , j = nums.length) => {
+  if (i === j && nums[i] === sum) return true;
+  if (i > j) return false;
+  if (nums.slice(i,j).reduce((a,el) => a + el, 0) === sum) return true;
+
+  return hasSubarraySum(nums,sum,i+1,j) || hasSubarraySum(nums,sum,i,j-1);
 };
-/*
-count from each side
-  if a reduction adds to the total, return true
-  if it never does, return false
-2 exit conditions:
-  i > j, return false
-  i === j === sum, return true
-p
-Number Array + Number
-r
-Boolean (does any SEQUENTIAL sequence of numbers add to sum)
-e
-empty array and 0 (return true?)
-negatives (should work with addition still)
-p
-*/
+
 
 console.log(hasSubarraySum([5], 5), true);
 console.log(hasSubarraySum([1, 3, 1, 4, 3], 8), true);
