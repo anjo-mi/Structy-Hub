fb4f1282079f372ea6f26094041e128251cc1ea2
Kyle Nojaim <nojaim.k@gmail.com>
2025-09-30 09:58:14 -0400

    Solved 'paired parentheses' (node)

--- Stack/123-paired-parentheses/paired-parentheses.js
+++ Stack/123-paired-parentheses/paired-parentheses.js
@@ -1,18 +1,20 @@
 const pairedParentheses = (str) => {
+  const opens  = { '{':'}' , '[':']' , '(':')' };
+  const closes = { '}':'{' , ']':'[' , ')':'(' };
   const stack = [];
   for (const char of str){
-    if (char === '(') stack.push(char);
-    if (char === ')'){
-      if (!stack.length) return false;
+    if (char in opens) stack.push(char);
+    if (char in closes){
+      if (stack[stack.length-1] !== closes[char]) return false;
       stack.pop();
     }
   }
   return !stack.length;
 };
 console.log(pairedParentheses("(david)((abby))"), true);
-console.log(pairedParentheses("()rose(jeff"), false);
 console.log(pairedParentheses(""), true);
 console.log(pairedParentheses("(())(water)()"), true);
+console.log(pairedParentheses("()rose(jeff"), false);
 module.exports = {
   pairedParentheses,
 };
