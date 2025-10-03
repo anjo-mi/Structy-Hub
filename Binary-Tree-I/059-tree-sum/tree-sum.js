3edbf7723cd8cd0e7f62b6b3342428b9c0d3189f
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-03 13:19:47 -0400

    Solved 'tree sum' (node)

--- Binary-Tree-I/059-tree-sum/tree-sum.js
+++ Binary-Tree-I/059-tree-sum/tree-sum.js
@@ -9,12 +9,12 @@ class Node {
 const treeSum = (root) => {
   let total = 0;
   if (!root) return total;
-  const s = [root];
-  while (s.length){
-    const curr = s.pop();
+  const q = [root];
+  while (q.length){
+    const curr = q.shift();
     total += curr.val;
-    if (curr.right) s.push(curr.right);
-    if (curr.left) s.push(curr.left);
+    if (curr.left) q.push(curr.left);
+    if (curr.right) q.push(curr.right);
   }
   return total;
 };
