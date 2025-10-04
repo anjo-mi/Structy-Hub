5fe783231ee7488d1b925cbaff817cd507fb2c47
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-04 10:21:09 -0400

    Solved 'has path' (node)

--- Graph-I/083-has-path/has-path.js
+++ Graph-I/083-has-path/has-path.js
@@ -1,11 +1,11 @@
 const hasPath = (graph, src, dst) => {
+  const v = new Set();
   const q = [src];
-  const v =  new Set();
   while (q.length){
     const curr = q.shift();
-    if (curr === dst) return true;
-    if(v.has(curr)) continue;
+    if (v.has(curr)) continue;
     v.add(curr);
+    if (curr === dst) return true;
     for (const n of graph[curr]) q.push(n);
   }
   return false;
