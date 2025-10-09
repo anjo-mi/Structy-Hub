103165e9754a8205b82c52d673f03f004bdcfbb9
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-09 00:47:12 -0400

    Solved 'decompress braces' (node)

--- Stack/125-decompress-braces/decompress-braces.js
+++ Stack/125-decompress-braces/decompress-braces.js
@@ -1,19 +1,16 @@
 const decompressBraces = (s) => {
   const arab = new Set('123456789');
-  let i = 0,
-      j = 0;
   const stack = [];
-  for (let i = 0 ; i < s.length ; i++){
+  for (let i = 0; i < s.length ; i++){
     if (s[i] === '{') stack.push(i);
     if (s[i] === '}'){
       const start = stack.pop();
-      const replaced = s.slice(start,i+1);
-      const exp = replaced.slice(1,-1);
-      let numInd = start;
-      while (arab.has(s[numInd-1])) numInd--;
-      const num = +s.slice(numInd,start);
-      const actualReplaced = s.slice(numInd,i+1);
-      s = s.replace(actualReplaced,exp.repeat(num));
+      const exp = s.slice(start+1,i);
+      let numIndex = start;
+      while (arab.has(s[numIndex-1])) numIndex--;
+      const num = +s.slice(numIndex,start);
+      const replaced = s.slice(numIndex,i+1);
+      s = s.replace(replaced,exp.repeat(num));
       i = s.lastIndexOf(exp) + exp.length - 1;
     }
   }
