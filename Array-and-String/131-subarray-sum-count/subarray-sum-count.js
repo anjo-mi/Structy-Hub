1627e3168838a1389f90dee8ba260f7f47fb1e2c
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-10 00:06:27 -0400

    Solved 'subarray sum count' (node)

--- Array-and-String/131-subarray-sum-count/subarray-sum-count.js
+++ Array-and-String/131-subarray-sum-count/subarray-sum-count.js
@@ -1,13 +1,12 @@
-const subarraySumCount = (nums,sum,i = 0, j = nums.length, memo = {}) => {
-  const k = i + ';' + j;
-  if (i > j) return memo[k] = 0;
-  if (i === j && nums[i] === sum) memo[k] = 1;
-  else memo[k] = nums.slice(i,j).reduce((a,el) => a + el,0) === sum ? 1 : 0;
-
+const subarraySumCount = (nums,sum,i=0,j=nums.length,memo={}) => {
+  const k = i +';'+ j;
+  if (k in memo) return;
+  if (i > j) return;
+  if (i === j && nums[i] === sum) return memo[k] = 1;
+  memo[k] = nums.slice(i,j).reduce((a,el) => a + el,0) === sum ? 1:0;
   subarraySumCount(nums,sum,i+1,j,memo);
   subarraySumCount(nums,sum,i,j-1,memo);
-
-  return Object.values(memo).reduce((a,el) => a + el,0);
+  return Object.values(memo).reduce((a,el) => a + el, 0);
 };
 console.log(subarraySumCount([1, 3, 1, 4, 3], 2), 0);
 console.log(subarraySumCount([1, 3, 1, 4, -2, 3], 5), 3);
