11fda506f74d10d43ecbce2c6e78b8f11d517229
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-12 10:30:57 -0400

    Solved 'valid compound' (node)

--- Dynamic-Programming/117-valid-compound/valid-compound.js
+++ Dynamic-Programming/117-valid-compound/valid-compound.js
@@ -1,11 +1,10 @@
-const validCompound = (compound, elements, i = 0, memo = {}) => {
+const validCompound = (compound, elements,i=0,memo = {}) => {
   elements = elements.map(el => el.toLowerCase());
   if (i === compound.length) return true;
   if (i  >  compound.length) return false;
   if (i in memo) return memo[i];
 
-  memo[i] = elements.some(el => compound.startsWith(el,i) && validCompound(compound,elements,i + el.length, memo));
-
+  memo[i] = elements.some(elm => compound.startsWith(elm,i) && validCompound(compound,elements,i+elm.length,memo));
   return memo[i];
 };
 console.log(validCompound("neco", [
