fe8cee0db01408e950d076a4cff09243e6090b48
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-19 13:39:58 -0400

    Solved 'undirected path' (node)

--- Graph-I/084-undirected-path/undirected-path.js
+++ Graph-I/084-undirected-path/undirected-path.js
@@ -14,7 +14,7 @@ const undirectedPath = (edges, src, dst) => {
     if (v.has(curr)) continue;
     v.add(curr);
     if (curr === dst) return true;
-    for (const n of graph[curr]) q.push(n);
+    for (const n of graph[curr]) q.push(n)
   }
   return false;
 };
