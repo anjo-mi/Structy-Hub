e708c9d03971e517f0f6091ab7b3548d2a9ca88b
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-20 13:02:54 -0400

    Solved 'middle value' (node)

--- Linked-List-II/141-middle-value/middle-value.js
+++ Linked-List-II/141-middle-value/middle-value.js
@@ -6,13 +6,12 @@ class Node {
 }
 
 const middleValue = (head) => {
-  let s = head,
-      f = head;
-  while (f && f.next){
-    s = s.next;
-    f = f.next.next;
-  }
-  return s.val;
+  if (!head) return null;
+  const arr = [];
+  let curr = head;
+  while (curr) {arr.push(curr.val); curr = curr.next}
+  const mid = Math.floor(arr.length / 2);
+  return arr[mid]
 };
 
 const a = new Node('a');
@@ -24,15 +23,15 @@ a.next = b;
 b.next = c;
 c.next = d;
 d.next = e;
-console.log(middleValue(a), {c});
+console.log(middleValue(a), 'c');
 const q = new Node('q');
-console.log(middleValue(q), q);
+console.log(middleValue(q), 'q');
 const x = new Node('x');
 const y = new Node('y');
 const z = new Node('z');
 x.next = y;
 y.next = z;
-console.log(middleValue(x), y);
+console.log(middleValue(x), 'y');
 
 module.exports = {
   middleValue,
