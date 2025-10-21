c13f648931e87663b995d2f10c66ff3edd059719
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-21 01:30:24 -0400

    Solved 'quickest concat' (node)

--- Dynamic-Programming/116-quickest-concat/quickest-concat.js
+++ Dynamic-Programming/116-quickest-concat/quickest-concat.js
@@ -1,15 +1,15 @@
-const quickestConcat = (s, words,i = 0, depth=0, memo = {}) => {
+const quickestConcat = (s, words,i = 0 ,depth = 0, memo = {}) => {
   if (i === s.length) return 0;
   if (i  >  s.length) return Infinity;
   if (i in memo) return memo[i];
 
   let min = Infinity;
   for (const w of words) if (s.startsWith(w,i)){
-    const poss = quickestConcat(s,words,i+w.length,depth+1,memo) + 1;
-    if (poss < min) min = poss;
+    const concat = quickestConcat(s,words,i+w.length,depth+1,memo) + 1;
+    if (concat < min) min = concat;
   }
   memo[i] = min;
-  return !isFinite(memo[i]) && !depth ? -1 : memo[i];
+  return !depth && !isFinite(memo[i]) ? -1 : memo[i];
 };
 
 console.log(quickestConcat('simchacindy', ['sim', 'simcha', 'acindy']), -1);
