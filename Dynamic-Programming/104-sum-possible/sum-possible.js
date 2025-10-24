ac89495f0754a9c468687dbc57aee4997df60ac3
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-24 00:43:01 -0400

    Solved 'sum possible' (node)

--- Dynamic-Programming/104-sum-possible/sum-possible.js
+++ Dynamic-Programming/104-sum-possible/sum-possible.js
@@ -1,5 +1,5 @@
-const sumPossible = (amt, nums,memo = {}) => {
-  if (amt === 0) return true;
+const sumPossible = (amt, nums, memo = {}) => {
+  if (!amt) return true;
   if (amt < 0) return false;
   if (amt in memo) return memo[amt];
 
