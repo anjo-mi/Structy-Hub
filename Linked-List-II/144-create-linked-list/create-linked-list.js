9ffb83387b02e891dae0759845cf06107e3bf885
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-25 14:15:39 -0400

    Solved 'create linked list' (node)

--- Linked-List-II/144-create-linked-list/create-linked-list.js
+++ Linked-List-II/144-create-linked-list/create-linked-list.js
@@ -8,36 +8,13 @@ class Node {
 const createLinkedList = (vals) => {
   if (!vals.length) return null;
   const head = new Node(vals.shift());
-  let curr = head,
-      i = 0;
-  while (i < vals.length){
-    curr.next = new Node(vals[i]);
-    curr = curr.next;
-    i++;
+  let tail = head;
+  for (const n of vals){
+    tail.next = new Node(n);
+    tail = tail.next
   }
   return head;
 };
-/*
-if (!vals.length) return null;
-const first = vals.shift();
-const head = new Node(node.val);
-let curr = head;
-let i = vals.length - 1
-while ( i < vals.length) i++
-{curr.next = new Node(vals[i])
-curr = curr.next;}
-return head
-p
-Array of any type (!null)
-r
-linked list of nodes (.val, .next || null)
-  follow order of items in array
-e
-empty list (return null)
-single list, return single item as node
-p
-(already have Node class)
-*/
 
 console.log(createLinkedList(["h", "e", "y"]), 'h -> e -> y' );
 console.log(createLinkedList([]), null);
