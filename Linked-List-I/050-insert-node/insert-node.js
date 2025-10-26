8f6a25ef365066358ffb714bcc10bb82d53b11c2
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-26 13:52:16 -0400

    Solved 'insert node' (node)

--- Linked-List-I/050-insert-node/insert-node.js
+++ Linked-List-I/050-insert-node/insert-node.js
@@ -8,15 +8,15 @@ class Node {
 const insertNode = (head, val, i) => {
   const n = new Node(val);
   if (!i){ n.next = head; return n; }
-  let curr = head,
-      prev = null;
-  while (curr && i){
-    prev = curr;
-    curr = curr.next;
+  let c = head,
+      p = null;
+  while (c && i){
+    p = c;
+    c = c.next;
     i--;
   }
-  prev.next = n;
-  n.next = curr;
+  p.next = n;
+  n.next = c;
   return head;
 };
 const a = new Node("a");
