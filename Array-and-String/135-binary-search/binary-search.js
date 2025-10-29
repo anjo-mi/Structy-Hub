4dfce8576a66955fbe6ff6baf618c5bf828b6453
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-29 12:04:09 -0400

    Solved 'binary search' (node)

--- Array-and-String/135-binary-search/binary-search.js
+++ Array-and-String/135-binary-search/binary-search.js
@@ -1,12 +1,12 @@
 const binarySearch = (nums, targ) => {
+  if (!nums.length) return -1;
   let i = 0,
-      j = nums.length-1;
-  while (i<=j){
-    const mid = Math.floor((i+j)/2);
-    const n = nums[mid];
-    if (n === targ) return mid;
-    if (n < targ) i = mid + 1;
-    if (n > targ) j = mid - 1;
+      j = nums.length -1;
+  while (i <= j){
+    const mid = Math.floor( (i+j) / 2 );
+    if (nums[mid] === targ) return mid;
+    if (nums[mid] > targ) j = mid - 1;
+    if (nums[mid] < targ) i = mid + 1;
   }
   return -1;
 };
