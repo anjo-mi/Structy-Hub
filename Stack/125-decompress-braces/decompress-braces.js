3540930894c99b1da2c3ac326e9c74abfd6a84c0
Kyle Nojaim <nojaim.k@gmail.com>
2025-10-31 00:43:14 -0400

    Solved 'decompress braces' (node)

--- Stack/125-decompress-braces/decompress-braces.js
+++ Stack/125-decompress-braces/decompress-braces.js
@@ -1,34 +1,24 @@
 const decompressBraces = (s) => {
-  const nums = '123456789';
+  const arab = new Set('0123456789');
   const stack = [];
-  for (let char of s) {
-    if (nums.includes(char)) {
-      stack.push(Number(char));
-    } else {
-      if (char === '}') {
-        let seg = '';
-        while (typeof stack[stack.length - 1] !== 'number') {
-          const pop = stack.pop();
-          seg = pop + seg;
-        }
-        const number = stack.pop();
-        stack.push(repeat(seg, number));
-      } else if (char !== '{') {
-        stack.push(char);
-      }
+  let i = 0;
+  while(i < s.length){
+    if (s[i] === '{') stack.push(i);
+    if (s[i] === '}'){
+      const start = stack.pop();
+      const brackets = s.slice(start,i+1);
+      const exp = s.slice(start+1,i);
+      let numInd = start;
+      while (arab.has(s[numInd-1])) numInd--;
+      const num = +s.slice(numInd, start);
+      s = s.replace(s.slice(numInd,i+1), exp.repeat(num));
+      i = s.lastIndexOf(exp) + exp.length - 1
     }
+    i++;
   }
-  
-  return stack.join('');
+  return s;
 };
 
-const repeat = (str, n) => {
-  let result = '';
-  for (let i = 0; i < n; i += 1) {
-    result += str;
-  }
-  return result;
-};
 console.log(decompressBraces("2{q}3{tu}v"), 'qqtututuv');
 console.log(decompressBraces("2{y3{o}}s"), 'yoooyooos');
 console.log(decompressBraces("2{3{r4{e}r}io}"), 'reeeerreeeerreeeerioreeeerreeeerreeeerio');
