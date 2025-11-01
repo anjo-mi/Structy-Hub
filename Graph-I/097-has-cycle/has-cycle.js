c0e80edb43e1118c7cf7566c7f10bc9f91303298
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-01 00:11:13 -0400

    Solved 'has cycle' (node)

--- Graph-I/097-has-cycle/has-cycle.js
+++ Graph-I/097-has-cycle/has-cycle.js
@@ -1,11 +1,11 @@
 const hasCycle = (graph) => {
   for (const node in graph){
     const v = new Set();
-    const q = [node];
+    const q = [node]
     while (q.length){
-      const curr = q.shift();
-      v.add(curr);
-      for (const n of graph[curr]){
+      const c = q.shift();
+      v.add(c)
+      for (const n of graph[c]){
         if (v.has(n)) return true;
         q.push(n);
       }
