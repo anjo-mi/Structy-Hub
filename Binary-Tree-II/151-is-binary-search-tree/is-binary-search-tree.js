676f439cbb033dfc2228ebcdf94f407c7a8d3a8f
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-03 09:54:39 -0500

    Solved 'is binary search tree' (node)

--- Binary-Tree-II/151-is-binary-search-tree/is-binary-search-tree.js
+++ Binary-Tree-II/151-is-binary-search-tree/is-binary-search-tree.js
@@ -6,40 +6,13 @@ class Node {
   }
 }
 
-// ibst (12,null,null)
-// ibst (5,12,12)
-
-const isBinarySearchTree = (root,low = null,high = null) => {
+const isBinarySearchTree = (root,h,l) => {
   if (!root) return true;
-  if (high || high === 0) if (root.val > high) return false;
-  if (low  || low  === 0) if (root.val < low ) return false;
-  
-  
-  return isBinarySearchTree(root.left,low,root.val) && isBinarySearchTree(root.right,root.val,high);
+  if (h || h === 0) if (root.val > h) return false;
+  if (l || l === 0) if (root.val < l) return false;
+  return isBinarySearchTree(root.left,root.val,l) && isBinarySearchTree(root.right,h,root.val);
 };
-// if !root: true (empty tree doesnt break criteria)
-// we should set the parameters to also take a low and a high
-// if (low or high are defined)
-//   - ensure .val is lower than high and higher than low
-//     - if not return false
-//     - if so: recurse on the right and left
-//     - right will get val as new high
-//     - left get val as new low
-/*
-taking in root of (possibly binary search tree)
-  nodes (.val + .left &&/|| .right or null)
-returning a Boolean  -> is this actually a BST?
 
-     12
-   /   \
-  5     18
- / \     \
-3   15     19
-this will be the main pain point
-  - false because 15 > 12, but would pass the immediate check of 15 > 5
-
-
-*/
 const a = new Node(12);
 const b = new Node(5);
 const c = new Node(18);
