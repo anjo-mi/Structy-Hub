bdd864e570cc2e38c552d5c26166f01dc949a6c7
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-08 00:27:35 -0500

    Solved 'sum possible' (node)

--- Dynamic-Programming/103-sum-possible/sum-possible.js
+++ Dynamic-Programming/103-sum-possible/sum-possible.js
@@ -3,17 +3,16 @@ const sumPossible = (amt, nums, memo = {}) => {
   if (amt < 0) return false;
   if (amt in memo) return memo[amt];
 
-  memo[amt] = nums.some(num => sumPossible(amt - num, nums, memo));
-  return memo[amt];
+  return memo[amt] = nums.some(n => sumPossible(amt - n,nums,memo));
 };
 /*
 
 */
 console.log(sumPossible(8, [5, 12, 4]), true);
-console.log(sumPossible(15, [6, 2, 10, 19]), false);
-console.log(sumPossible(12, []), false);
 console.log(sumPossible(0, []), true);
 console.log(sumPossible(13, [3, 5]), true);
+console.log(sumPossible(15, [6, 2, 10, 19]), false);
+console.log(sumPossible(12, []), false);
 
 module.exports = {
   sumPossible,
