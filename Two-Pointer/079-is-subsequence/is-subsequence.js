de95ca5d2a0e6cff0f42ea78ef1106f8e058e676
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-08 00:08:20 -0500

    Solved 'is subsequence' (node)

--- Two-Pointer/079-is-subsequence/is-subsequence.js
+++ Two-Pointer/079-is-subsequence/is-subsequence.js
@@ -2,10 +2,8 @@ const isSubsequence = (s1,s2) => {
   let i = 0,
       j = 0;
   while (i < s1.length && j < s2.length){
-    if (s1[i] === s2[j]){
-      i++;
-      j++;
-    }else j++;
+    if (s1[i] === s2[j]) i++;
+    j++;
   }
   return i >= s1.length;
 };
