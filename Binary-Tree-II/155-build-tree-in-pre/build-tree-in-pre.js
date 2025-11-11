b57a1f215002fe43fdbe0bc729090558ce517bc9
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-11 13:45:49 -0500

    Solved 'build tree in pre' (node)

--- Binary-Tree-II/155-build-tree-in-pre/build-tree-in-pre.js
+++ Binary-Tree-II/155-build-tree-in-pre/build-tree-in-pre.js
@@ -6,19 +6,20 @@ class Node {
   }
 }
 
-const buildTreeInPre = (inn, pre, pos = {}) => {
-  if (!inn.length) return null;
+const buildTreeInPre = (inn, pre) => {
+  if (!inn.length || !pre.length) return null;
   const r = pre.shift();
   const innDex = inn.indexOf(r);
   const leftInn = inn.slice(0,innDex);
-  const rightInn = inn.slice(innDex + 1);
+  const rightInn = inn.slice(innDex+1);
   const leftPre = pre.slice(0,leftInn.length);
   const rightPre = pre.slice(leftInn.length, leftInn.length + rightInn.length);
   const root = new Node(r);
-  root.left = buildTreeInPre(leftInn,leftPre,pos);
-  root.right = buildTreeInPre(rightInn,rightPre,pos);
+  root.left = buildTreeInPre(leftInn,leftPre);
+  root.right = buildTreeInPre(rightInn,rightPre);
   return root;
 };
+
 console.log(buildTreeInPre(
   [ 'd', 'b', 'g', 'e', 'h', 'a', 'c', 'f' ],
   [ 'a', 'b', 'd', 'e', 'g', 'h', 'c', 'f' ] 
