9f2abcdfc471aa8ce9eb828c37a9b03c4e54d417
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-12 00:04:40 -0500

    Solved 'overlap subsequence' (node)

--- Dynamic-Programming/113-overlap-subsequence/overlap-subsequence.js
+++ Dynamic-Programming/113-overlap-subsequence/overlap-subsequence.js
@@ -1,12 +1,11 @@
 const overlapSubsequence = (s1,s2,i=0,j=0,memo={}) => {
-  if (i === s1.length) return 0;
-  if (j === s2.length) return 0;
   const k = i+';'+j;
   if (k in memo) return memo[k];
+  if (i >= s1.length || j >= s2.length) return 0;
   if (s1[i] === s2[j]) return memo[k] = 1 + overlapSubsequence(s1,s2,i+1,j+1,memo);
-  else return memo[k] = Math.max(
+  return memo[k] = Math.max(
     overlapSubsequence(s1,s2,i+1,j,memo),
-    overlapSubsequence(s1,s2,i,j+1,memo)
+    overlapSubsequence(s1,s2,i,j+1,memo),
   )
 };
 console.log(overlapSubsequence("xcyats", "criaotsi"), 4);
