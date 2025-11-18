8c85242e4c8c0b0cdcc62c47fc42adb039fc4df7
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-18 11:07:48 -0500

    Solved 'heap insertion' (node)

--- Heap/160-heap-insertion/heap-insertion.js
+++ Heap/160-heap-insertion/heap-insertion.js
@@ -11,16 +11,22 @@ class MinHeap {
     return this.array.length;
   }
 
+  swap(i1,i2){
+    [this.array[i1] , this.array[i2]] = [this.array[i2] , this.array[i1]];
+  }
+
+  siftUp(i){
+    const pi = Math.floor((i - 1) / 2);
+    if (this.array[i] < this.array[pi]){
+      this.swap(i,pi);
+      i = pi;
+    }else return;
+    this.siftUp(i);
+  }
+
   insert(val) {
     this.array.push(val);
-    let i = this.size() - 1;
-    while (i){
-      const pi = Math.floor((i-1) / 2);
-      if (this.array[i] < this.array[pi]){
-        [ this.array[i] , this.array[pi] ] = [ this.array[pi] , this.array[i] ];
-      }else break;
-      i = pi;
-    }
+    this.siftUp(this.array.length - 1);
   }
 }
 
