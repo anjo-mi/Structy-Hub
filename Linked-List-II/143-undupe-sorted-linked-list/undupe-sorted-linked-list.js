0eeae47d71ed650a40c9ac073d14d67e94534960
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-18 11:22:35 -0500

    Solved 'undupe sorted linked list' (node)

--- Linked-List-II/143-undupe-sorted-linked-list/undupe-sorted-linked-list.js
+++ Linked-List-II/143-undupe-sorted-linked-list/undupe-sorted-linked-list.js
@@ -7,11 +7,11 @@ class Node {
 
 const undupeSortedLinkedList = (head) => {
   if (!head) return null;
-  let curr = head.next,
-      prev = head;
+  let prev = head,
+      curr = head.next;
   while (curr){
     if (prev.val === curr.val) prev.next = curr.next;
-    else prev = curr
+    else prev = curr;
     curr = curr.next;
   }
   return head;
