b3d2999a5b1d87fa4c372b737f6f3e6973aaf6b5
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-18 11:55:34 -0500

    Solved 'build tree in post' (node)

--- Binary-Tree-II/154-build-tree-in-post/build-tree-in-post.js
+++ Binary-Tree-II/154-build-tree-in-post/build-tree-in-post.js
@@ -8,16 +8,16 @@ class Node {
 
 const buildTreeInPost = (inn, post) => {
   if (!inn.length) return null;
-  const r = post.pop();
-  const innDex = inn.indexOf(r);
+  const root = post.pop();
+  const innDex = inn.indexOf(root);
   const leftInn = inn.slice(0,innDex);
   const rightInn = inn.slice(innDex + 1);
   const leftPost = post.slice(0,leftInn.length);
   const rightPost = post.slice(leftInn.length);
-  const root = new Node(r);
-  root.left = buildTreeInPost(leftInn,leftPost);
-  root.right = buildTreeInPost(rightInn,rightPost);
-  return root;
+  const r = new Node(root);
+  r.left = buildTreeInPost(leftInn,leftPost);
+  r.right = buildTreeInPost(rightInn,rightPost);
+  return r;
 };
 console.log(buildTreeInPost(
   [ 'd', 'b', 'e', 'a', 'f', 'c', 'g' ],
