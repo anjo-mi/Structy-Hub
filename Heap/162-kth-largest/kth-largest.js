dc6121af8f3eff1adcb741a7d4bad2aaa147a199
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-22 16:56:37 -0500

    Solved 'kth-largest' (node)

--- Heap/162-kth-largest/kth-largest.js
+++ Heap/162-kth-largest/kth-largest.js
@@ -1,5 +1,5 @@
 const kthLargest = (numbers, k) => {
-  return numbers.sort((a,b) => b-a)[k-1];
+  return numbers.slice(0).sort((a,b) => b-a)[k-1];
 };
 
 console.log(kthLargest([9,2,6,6,1,5,8,7], 3), 7);
