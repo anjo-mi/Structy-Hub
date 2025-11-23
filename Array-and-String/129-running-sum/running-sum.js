b5287b790c8f72bbcba6cefb41ee2114b33c002c
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-23 00:23:28 -0500

    Solved 'running sum' (node)

--- Array-and-String/129-running-sum/running-sum.js
+++ Array-and-String/129-running-sum/running-sum.js
@@ -1,7 +1,8 @@
 const runningSum = (nums) => {
-  let ttoal = 0 ;
-  return nums.map(n => ttoal += n);
+  let sum = 0 ;
+  return nums.map(n => sum += n);
 };
+
 console.log(runningSum([4, 2, 1, 6, 3, 6]), [ 4, 6, 7, 13, 16, 22 ]);
 console.log(runningSum([12, 88, 0, -50, 30, 2]), [ 12, 100, 100, 50, 80, 82 ]);
 console.log(runningSum([]), [ ]);
