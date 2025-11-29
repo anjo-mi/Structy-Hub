e2e6274c4bc61d88f59f38f617708495157462c1
Kyle Nojaim <nojaim.k@gmail.com>
2025-11-29 13:26:13 -0500

    Solved 'subsets' (node)

--- Exhaustive-Recursion/167-subsets/subsets.js
+++ Exhaustive-Recursion/167-subsets/subsets.js
@@ -1,10 +1,9 @@
 const subsets = (elms) => {
   if (!elms.length) return [[]];
   const first = elms[0];
-
   const subsWithout = subsets(elms.slice(1));
-  const subsWith = subsWithout.map(s => [first,...s]);
-  return [...subsWith,...subsWithout];
+  const subsWith = subsWithout.map(sub => [first,...sub]);
+  return [...subsWith, ...subsWithout];
 };
 
 
