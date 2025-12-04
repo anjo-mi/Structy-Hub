9fb88b84768af2a5980962ab16bf55120049a353
Kyle Nojaim <nojaim.k@gmail.com>
2025-12-04 10:14:05 -0500

    Solved 'all tree paths' (node)

--- Binary-Tree-I/068-all-tree-paths/all-tree-paths.js
+++ Binary-Tree-I/068-all-tree-paths/all-tree-paths.js
@@ -7,24 +7,26 @@ class Node {
 }
 
 const allTreePaths = root => {
+  return _allTreePaths(root).map(p => p.reverse());
+}
+
+const _allTreePaths = r =>{
   const paths = [];
-  if (!root) return paths;
-  const s = [{n:root, path:[root.val]}];
-  while (s.length){
-    const {n,path} = s.pop();
-    if (!n.left && !n.right) paths.push(path);
-    if (n.right) s.push({n:n.right,path:path.concat(n.right.val)});
-    if (n.left) s.push({n:n.left,path:path.concat(n.left.val)});
+  if (!r) return paths;
+  if (!r.right && !r.left) return [[r.val]];
+  const left = _allTreePaths(r.left);
+  const right = _allTreePaths(r.right);
+  for (const p of left){
+    p.push(r.val);
+    paths.push(p);
+  }
+  for (const p of right){
+    p.push(r.val);
+    paths.push(p);
   }
   return paths;
 }
 
-
-
-
-/*
-
-*/
 const a = new Node('a');
 const b = new Node('b');
 const c = new Node('c');
