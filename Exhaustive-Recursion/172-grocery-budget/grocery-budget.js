5c4eb4cc355b2be04874127f09c0995e4834d7ac
Kyle Nojaim <nojaim.k@gmail.com>
2025-12-04 18:45:52 -0500

    Solved 'grocery budget' (node)

--- Exhaustive-Recursion/172-grocery-budget/grocery-budget.js
+++ Exhaustive-Recursion/172-grocery-budget/grocery-budget.js
@@ -4,23 +4,19 @@ const groceryBudget = (list, budget) => {
 
   const [first,price] = list[0];
   const rest = list.slice(1);
-  const res = [];
-  
-  const restWithAdjustedPrice = groceryBudget(rest,budget - price);
-  const restWithoutAdjustedPrice = groceryBudget(rest,budget);
-  
-  restWithAdjustedPrice.forEach(l =>{
+  const possCombos = [];
+
+  const wItem = groceryBudget(rest,budget - price);
+  wItem.forEach(l => {
     if (budget - price >= 0){
       l.push(first);
-      res.push(l);
+      possCombos.push(l);
     }
-  });
-  
-  restWithoutAdjustedPrice.forEach(l =>{
-    res.push(l);
-  });
-  
-  return res;
+  })
+  const woItem = groceryBudget(rest,budget);
+  woItem.forEach(l => possCombos.push(l));
+
+  return possCombos;
 };
 
 console.log(groceryBudget([  
@@ -29,16 +25,19 @@ console.log(groceryBudget([
   ['butter', 3],
   ['garlic', 1],
 ], 7));
+
 console.log(groceryBudget([  
   ['eggs', 5],
   ['milk', 3],
   ['butter', 3],
 ], 7));
+
 console.log(groceryBudget([  
   ['eggs', 5],
   ['milk', 3],
   ['butter', 3],
 ], 20));
+
 console.log(groceryBudget([  
   ['salt', 1],
   ['apples', 5],
