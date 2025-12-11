d55948f3a4b5a54cead02bf9a3c3530bb045ec4f
Kyle Nojaim <nojaim.k@gmail.com>
2025-12-11 00:33:19 -0500

    Solved 'possible paths' (node)

--- Exhaustive-Recursion/174-possible-paths/possible-paths.js
+++ Exhaustive-Recursion/174-possible-paths/possible-paths.js
@@ -4,26 +4,26 @@ const possiblePaths = (graph, src, dst) => {
 
   while (q.length){
     const {node,path} = q.shift();
-    // path.push(node);
+
     if (node === dst){
       allPaths.push(path);
     }
     if (!graph[node].length) continue;
-    
-    console.log({node,path,dst,allPaths})
-    console.log(graph[node])
+
     for (const n of graph[node]){
       q.push({ node: n , path: [...path,n] });
     }
   }
   return allPaths;
 };
+
 const graph = {
   "a": ["b", "c", "d"],
   "b": ["d"],
   "c": ["d"],
   "d": []
 };
+
 console.log(possiblePaths(graph, "a", "d")); // ->
 // [
 //   ["a", "b", "d"],
@@ -31,18 +31,6 @@ console.log(possiblePaths(graph, "a", "d")); // ->
 //   ["a", "d"]
 // ]
 
-/*
-getDists, but make it towards a specific destination, from a specific origin
-
-set src to arr [src]
-make a queue of objects with node being initial src, + path
-  - if no length of graph[curr] continue the loop
-  push curr to path
-  if (curr === dst) push path to allPaths
-  otherwise push all nodes and their current paths to q
-
-return allPaths?
-*/
 
 // const grap = {
 //   "a": ["b", "c", "d"],
