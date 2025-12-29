95d59162dca122b2a7c0e8387b00979fcf7eded5
Kyle Nojaim <nojaim.k@gmail.com>
2025-12-29 15:53:18 -0500

    Solved 'quickest concat' (node)

--- Dynamic-Programming/114-quickest-concat/quickest-concat.js
+++ Dynamic-Programming/114-quickest-concat/quickest-concat.js
@@ -9,10 +9,8 @@ const quickestConcat = (s, words, i = 0 , memo = {}, d = 0) => {
   }
   return memo[i] = !isFinite(min) && !(d) ? -1 : min;
 };
-console.log(quickestConcat('simchacindy', ['sim', 'simcha', 'acindy']), -1);
+
 console.log(quickestConcat('caution', ['ca', 'ion', 'caut', 'ut']), 2);
-console.log(quickestConcat('respondorreact', ['re', 'or', 'spond', 'act', 'respond']), 4);
-console.log(quickestConcat('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', ['u', 'uu', 'uuu', 'uuuu', 'uuuuu']), 7);
 console.log(quickestConcat('rongbetty', ['wrong', 'bet']), -1);
 
 module.exports = {
