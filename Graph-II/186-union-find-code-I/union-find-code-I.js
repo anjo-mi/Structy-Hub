32da4cea98188a2889bc0d65503ec0d21be2ac16
Kyle Nojaim <nojaim.k@gmail.com>
2026-01-04 11:19:51 -0500

    Solved 'union find code I' (node)

--- Graph-II/186-union-find-code-I/union-find-code-I.js
+++ Graph-II/186-union-find-code-I/union-find-code-I.js
@@ -4,11 +4,10 @@ const countComponents = (n, edges) => {
     while (arr[b] !== b) b = arr[b];
     while (arr[a] !== a) a = arr[a];
     arr[b] = a;
-    console.log({arr,b})
   }
-  console.log({arr})
   return arr.reduce((a,el,i) => a += el === i ? 1 : 0 , 0);
 };
+
 console.log(countComponents(6, [
   [0, 3],
   [5, 3],
