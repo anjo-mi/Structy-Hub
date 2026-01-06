14fb4f7a5f1ba3956eb3234f4fc38f8f205b1d84
Kyle Nojaim <nojaim.k@gmail.com>
2026-01-06 16:00:50 -0500

    Solved 'extra cable' (node)

---
+++ Graph-II/189-extra-cable/extra-cable.js
@@ -0,0 +1,68 @@
+const find = (node,roots) => {
+  if (node === roots[node]) return node;
+  const found = find(roots[node], roots);
+  roots[node] = found;
+  return found;
+}
+
+const union = (a,b,roots,sizes) => {
+  const ra = find(a,roots);
+  const rb = find(b,roots);
+  if (ra === rb) return false;
+  if (sizes[ra] >= sizes[rb]){
+    roots[rb] = ra;
+    sizes[ra] += sizes[rb];
+  }else{
+    roots[ra] = rb;
+    sizes[rb] += sizes[ra];
+  }
+  return true;
+}
+
+const extraCable = (comps, cables) => {
+  const roots = Array(comps).fill(null).map((el,i) => i);
+  const sizes = Array(comps).fill(null).map(() => 1);
+  for (const [a,b] of cables){
+    console.log({a,b,roots,sizes});
+    if (!union(a,b,roots,sizes)) return [a,b];
+  }
+  return true;
+};
+
+console.log(extraCable(6, [
+  [0, 4],
+  [4, 5],
+  [3, 4],
+  [5, 1],
+  [1, 3],
+  [2, 4],
+]), [4, 3]);
+/*
+use the union find methods
+
+iterate thru all cables using those methods ^^
+
+if union returns false
+  it means weve encountered this node, and it points to a different endpoint
+    - this node has been encountered,
+        and has an extra 'edge' pointing to the same node
+*/
+
+// console.log(extraCable(5, [
+//   [3, 2],
+//   [1, 2],
+//   [4, 2],
+//   [3, 1],
+//   [0, 1],
+// ]), [3, 1])
+
+// console.log(extraCable(3, [
+//   [0, 1],
+//   [1, 2],
+//   [2, 0],
+// ]), [2, 0]);
+
+
+module.exports = {
+  extraCable,
+};
