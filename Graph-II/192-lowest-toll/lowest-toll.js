2aa07cae0a82cb5b2071228a3ab997dca5580fe8
Kyle Nojaim <nojaim.k@gmail.com>
2026-01-08 14:26:40 -0500

    Solved 'lowest toll' (node)

---
+++ Graph-II/192-lowest-toll/lowest-toll.js
@@ -0,0 +1,46 @@
+const buildRoutes = (highwayTolls) => {
+  const routes = {};
+  for (const [a,b,c] of highwayTolls){
+    routes[b] = routes[b] || {}
+    routes[a] = routes[a] || {}
+    routes[b][a] = c;
+    routes[a][b] = c;
+  }
+  return routes;
+}
+
+const findMin = (routes,a,b,v = new Set()) => {
+  if (a === b) return 0;
+  if (v.has(a)) return Infinity;
+  v.add(a);
+  let min = Infinity;
+  for (const child in routes[a]){
+    const cost = routes[a][child];
+    min = Math.min(
+      min,
+      cost + findMin(routes,child,b,v)
+    );
+  }
+  v.delete(a);
+  return min;
+}
+
+const lowestToll = (tolls, start, end) => {
+  const routes = buildRoutes(tolls);
+  return +findMin(routes,start,end).toFixed(1);
+};
+
+const highwayTolls = [
+  ["Hampton", "Fairfax", 7.50],
+  ["Roanoake", "Alexandria", 4.20],
+  ["Alexandria", "Hampton", 14.50],
+  ["Hampton", "Roanoake", 8.90],
+  ["Alexandria", "Fairfax", 5.90],
+  ["Hampton", "Manassas", 3.50],
+  ["Fairfax", "Manassas", 2.20],
+];
+console.log(lowestToll(highwayTolls, "Alexandria", "Hampton"), 11.60);
+
+module.exports = {
+  lowestToll,
+};
