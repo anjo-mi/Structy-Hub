32c00cc63d4df45140bd58a2f18595a2b56732d2
Kyle Nojaim <nojaim.k@gmail.com>
2026-01-20 20:31:13 -0500

    Solved 'breaking boundaries' (node)

---
+++ Mixed-Recall/209-breaking-boundaries/breaking-boundaries.js
@@ -0,0 +1,24 @@
+const inBounds = (rs,cs,r,c) => r < rs &&
+                                r >= 0 &&
+                                c < cs &&
+                                c >= 0;
+const dirs = [
+  [1,0], [-1,0], [0,1], [0,-1]
+];
+
+const breakingBoundaries = (rs, cs, k, r, c,memo ={}) => {
+  const key = k + ';' + r + ';' + c;
+  if (key in memo) return memo[key];
+  if (!inBounds(rs,cs,r,c)) return 1;
+  if (!k) return inBounds(rs,cs,r,c) ? 0 : 1;
+  let total = 0;
+  for (const [dr,dc] of dirs){
+    total += breakingBoundaries(rs,cs,k-1,r+dr,c+dc,memo);
+  }
+  return memo[key] = total;
+};
+console.log(breakingBoundaries(3, 4, 3, 0, 0), 11);
+
+module.exports = {
+  breakingBoundaries,
+};
