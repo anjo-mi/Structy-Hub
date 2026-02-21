fae757a917c8b6e24b85812e3bde9d48e48a6bde
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-21 00:20:39 -0500

    Solved 'all tree paths' (node)

---
+++ Binary-Tree-I/069-all-tree-paths/all-tree-paths.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const allTreePaths = root => {
+  return _allTreePaths(root).map(p => p.reverse());
+}
+
+const _allTreePaths = r =>{
+  const paths = [];
+  if (!r) return paths;
+  if (!r.right && !r.left) return [[r.val]];
+  const left = _allTreePaths(r.left);
+  const right = _allTreePaths(r.right);
+  for (const p of left){
+    p.push(r.val);
+    paths.push(p);
+  }
+  for (const p of right){
+    p.push(r.val);
+    paths.push(p);
+  }
+  return paths;
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(allTreePaths(a),  [ 
+   [ 'a', 'b', 'd' ], 
+   [ 'a', 'b', 'e' ], 
+   [ 'a', 'c', 'f' ] 
+ ]  );
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+module.exports = {
+  allTreePaths,
+};
