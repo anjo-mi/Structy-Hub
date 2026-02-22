85809ed10d04cf3fc08487c5b746c36246695274
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-22 09:54:36 -0500

    Solved 'nesting score' (node)

---
+++ Stack/135-nesting-score/nesting-score.js
@@ -0,0 +1,58 @@
+const nestingScore = (str) => {
+  const s = [0];
+  for (const char of str){
+    if (char === '[') s.push(0);
+    if (char === ']'){
+      const c = s.pop();
+      if (!c) s[s.length - 1]++;
+      else s[s.length-1] += c * 2;
+    }
+  }
+  return s.pop();
+};
+
+
+console.log(nestingScore("[][[][]][[]]"), 7);
+console.log(nestingScore("[]"), 1);
+console.log(nestingScore("[][][]"), 3);
+console.log(nestingScore("[[][][]]"), 6);
+console.log(nestingScore("[[[[[[[][]]]]]]][]"), 129);
+/*
+[[[[[[[][]]]]]]]
+[[[[[[1+1]]]]]]
+[[[[[[2]]]]]]
+[[[[[4]]]]]
+[[[[8]]]]
+[[[16]]]
+[[32]]
+[64]
+128
+*/
+
+
+
+/*
+p
+String ( consisting of only '[' , ']' )
+r
+Number: total "score" of brackets
+- any empty bracket is worth one point
+- any sequential pair of brackets is worth 1 point each
+- any bracket containing brackets within, multiplies their score by two
+    ( [ [1] ] ) // 2 ,
+    ( [ [1] [1] ] ) // 4 ,
+      exterior bracket multiplies interior by 2
+      (not counted on its own)
+e
+above*
+
+
+*/
+
+
+
+
+
+module.exports = {
+  nestingScore,
+};
