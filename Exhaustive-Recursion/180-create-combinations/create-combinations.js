2b8adccf6f2806fb044755205286ba73f7848ba3
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-22 09:50:53 -0500

    Solved 'create combinations' (node)

---
+++ Exhaustive-Recursion/180-create-combinations/create-combinations.js
@@ -0,0 +1,18 @@
+const createCombinations = (arr, k) => {
+  if (arr.length < k) return [];
+  if (!k) return [[]];
+  const combos = [];
+  const first = arr[0];
+
+  const remsWith = createCombinations(arr.slice(1), k-1);
+  const remsWithout = createCombinations(arr.slice(1), k);
+  for (const c of remsWith) combos.push([first,...c]);
+  return [...combos,...remsWithout];
+};
+
+
+console.log(createCombinations(['q', 'r', 's', 't'], 3));
+
+module.exports = {
+  createCombinations,
+};
