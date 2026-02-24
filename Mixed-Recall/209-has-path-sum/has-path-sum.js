234e9942d880dd42ae86fd5651c623edbce22b56
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-24 00:15:36 -0500

    Solved 'has path sum' (node)

---
+++ Mixed-Recall/209-has-path-sum/has-path-sum.js
@@ -0,0 +1,18 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const hasPathSum = (root, target, total = 0) => {
+  if (!root) return false;
+  total += root.val;
+  if (!root.right && !root.left) return total === target;
+  return hasPathSum(root.right,target,total) || hasPathSum(root.left,target,total);
+};
+
+module.exports = {
+  hasPathSum,
+};
