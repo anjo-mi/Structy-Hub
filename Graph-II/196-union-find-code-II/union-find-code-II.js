9c9038cca352bbe6b78ff83c639fbc5464181f31
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-25 11:21:05 -0500

    Solved 'union find code II' (node)

---
+++ Graph-II/196-union-find-code-II/union-find-code-II.js
@@ -0,0 +1,42 @@
+const find = (node, roots) => {
+  while (node !== roots[node]) node = find(roots[node],roots);
+  return node;
+}
+
+const union = (a,b,roots,sizes) => {
+  const ra = find(a,roots);
+  const rb = find(b,roots);
+  if (ra === rb) return;
+  if (ra > rb){
+    roots[rb] = ra;
+    sizes[ra] += sizes[rb];
+  }else{
+    roots[ra] = rb;
+    sizes[rb] += sizes[ra];
+  }
+}
+
+const countComponents = (n, edges) => {
+  const roots = Array(n).fill(null).map((el,i) => i);
+  const sizes = Array(n).fill(null).map(() => 1);
+  for (const [a,b] of edges){
+    union(a,b,roots,sizes);
+  }
+  return roots.reduce((a,el,i) => a += el === i ? 1 : 0 , 0);
+};
+
+console.log(countComponents(10, [
+  [3, 2],
+  [5, 4],
+  [4, 3],
+  [2, 1],
+  [0, 1],
+  [8, 9],
+  [5, 6],
+  [7, 8]
+]), 2);
+
+
+module.exports = {
+  countComponents,
+};
