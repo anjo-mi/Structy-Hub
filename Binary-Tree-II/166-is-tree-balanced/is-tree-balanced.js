1370c8162f55542162ec520297fc3daf78ecafcd
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-25 11:16:28 -0500

    Solved 'is tree balanced' (node)

---
+++ Binary-Tree-II/166-is-tree-balanced/is-tree-balanced.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const checkBalance = (r) => {
+  if (!r) return 0;
+
+  const left = checkBalance(r.left);
+  const right = checkBalance(r.right);
+  if (left < 0 || right < 0 || Math.abs(left - right) > 1) return -1;
+
+  return Math.max(left,right) + 1;
+}
+
+const isTreeBalanced = (r) => {
+  return checkBalance(r) >= 0;
+};
+
+
+
+const s = new Node("s");
+const t = new Node("t");
+const u = new Node("u");
+const v = new Node("v");
+const x = new Node("x");
+const y = new Node("y");
+s.left = t;
+s.right = u;
+t.left = v;
+u.right = x;
+v.left = y;
+console.log(isTreeBalanced(s), false);
+//        s
+//      /   \
+//     t     u
+//    /      \
+//   v        x
+//  /
+// y
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+console.log(isTreeBalanced(a), true);
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /
+//   g
+
+
+
+module.exports = {
+  isTreeBalanced,
+};
