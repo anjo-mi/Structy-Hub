0dd4b8b9856c1eedeb1e1daae7c4b8772bb24136
Kyle Nojaim <nojaim.k@gmail.com>
2026-03-23 11:44:42 -0400

    Solved 'token transform' (node)

--- Mixed-Recall/220-token-transform/token-transform.js
+++ Mixed-Recall/220-token-transform/token-transform.js
@@ -19,14 +19,6 @@ const tokenTransform = (s, tokens) => {
   while (s.includes('$')) s = tokenReplace(s,tokens);
   return s;
 };
-const tokens = {
-  $ADJECTIVE_1$: "quick",
-  $ADJECTIVE_2$: "eager",
-  $ADVERBS$: "$ADJECTIVE_1$ly and $ADJECTIVE_2$ly",
-  $VERB$: "hopped $DIRECTION$",
-  $DIRECTION$: "North",
-};
-console.log(tokenTransform("the $ADJECTIVE_1$ fox $ADVERBS$ $VERB$ward", tokens), '\n' + 'the quick fox quickly and eagerly hopped Northward');
 
 module.exports = {
   tokenTransform,
