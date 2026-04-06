57bd05b4538f1d4730d077dbcc9f34a3b99d96f3
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-06 11:15:20 -0400

    Solved 'topological order' (node)

---
+++ Graph-II/216-topological-order/topological-order.js
@@ -0,0 +1,32 @@
+const getHeights = (graph,node,levels) => {
+  if (!graph[node].length) levels[node] = 0;
+  else{
+    const max = Math.max(...graph[node].map(n => getHeights(graph,n,levels)));
+    levels[node] = max + 1;
+  }
+  return levels[node];
+};
+
+const topologicalOrder = (graph) => {
+  const levels = {};
+  for (const node in graph) getHeights(graph,node,levels);
+  return Object.keys(levels).sort((a,b) => levels[b] - levels[a]);
+};
+
+console.log(topologicalOrder({
+  h: ["l", "m"],
+  i: ["k"],
+  j: ["k", "i"],
+  k: ["h", "m"],
+  l: ["m"],
+  m: [],
+}), ['j', 'i', 'k', 'h', 'l', 'm']);
+console.log(topologicalOrder({
+  q: [],
+  r: ["q"],
+  s: ["r"],
+  t: ["s"],
+}), ['t', 's', 'r', 'q']);
+module.exports = {
+  topologicalOrder,
+};
