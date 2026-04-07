b0c5362a55450f86f094fb48ce6369b6695066b9
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-07 10:51:57 -0400

    Solved 'can color' (node)

---
+++ Graph-II/213-can-color/can-color.js
@@ -0,0 +1,44 @@
+const canColor = (graph) => {
+  const colored = {};
+  for (const node in graph){
+    let color = true;
+    if (node in colored) color = colored[node];
+    const q = [{node, color}];
+    while (q.length){
+      const {node,color} = q.shift();
+      if (node in colored && colored[node] !== color) return false;
+      else if (node in colored) continue;
+      colored[node] = color;
+      for (const n of graph[node]) {
+        // colored[n] = !color;
+        q.push({node:n, color: !color});
+      }
+    }
+  }
+  return true;
+};
+
+console.log(canColor({
+  x: ["y"],
+  y: ["x","z"],
+  z: ["y"]
+}), true);
+
+console.log(canColor({
+  q: ["r", "s"],
+  r: ["q", "s"],
+  s: ["r", "q"]
+}), false);
+
+console.log(canColor({
+  "a": ["b", "d"], 
+  "c": ["b", "f"], 
+  "b": ["a", "c"], 
+  "d": ["a", "e"], 
+  "e": ["d", "f"], 
+  "f": ["e", "c"]
+}), true);
+
+module.exports = {
+  canColor,
+};
