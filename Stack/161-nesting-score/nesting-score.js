affe9054851cd30a7c6fa6b93c7f60775c6092b2
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-08 11:05:40 -0400

    Solved 'nesting score' (node)

---
+++ Stack/161-nesting-score/nesting-score.js
@@ -0,0 +1,23 @@
+const nestingScore = (str) => {
+  const stack = [0];
+  for (const brack of str){
+    if (brack === '[') stack.push(0);
+    else if(brack === ']'){
+      const count = stack.pop();
+      if (!count) stack[stack.length - 1] += 1;
+      else stack[stack.length-1] += count * 2;
+    }
+  }
+  return stack.pop();
+};
+
+console.log(nestingScore("[[[[[[[][]]]]]]][]"), 129);
+
+
+
+
+
+
+module.exports = {
+  nestingScore,
+};
