9bfd8ff7182e17a93bb34b6e7cd1c48d0d268158
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-09 00:46:28 -0400

    Solved 'subarray sum count' (node)

---
+++ Array-and-String/166-subarray-sum-count/subarray-sum-count.js
@@ -0,0 +1,24 @@
+const subarraySumCount = (nums,targ) => {
+  const sums = [0];
+  let total = 0;
+  for (const num of nums) sums.push(total += num);
+
+  let count = 0;
+  const seen = {};
+  for (const sum of sums){
+    const comp = sum - targ;
+    if (comp in seen) count += seen[comp];
+    seen[sum] = (seen[sum] || 0) + 1;
+  }
+  return count;
+};
+
+
+console.log(subarraySumCount([1, 3, 1, 4, 3], 2), 0);
+console.log(subarraySumCount([1, 3, 1, 4, -2, 3], 5), 3);
+console.log(subarraySumCount([-2, 1, 1, 1, -1, 1, 1, 1, 1], 3), 8);
+
+
+module.exports = {
+  subarraySumCount,
+};
