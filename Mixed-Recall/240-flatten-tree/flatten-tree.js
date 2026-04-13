e0943a6d2c168ea8592b327e8ad1b7e4dda7c48a
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-13 00:32:33 -0400

    Solved 'flatten tree' (node)

--- Mixed-Recall/240-flatten-tree/flatten-tree.js
+++ Mixed-Recall/240-flatten-tree/flatten-tree.js
@@ -25,37 +25,6 @@ const flattenTree = (root) => {
 };
 
 
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
-
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-
-//      a
-//    /   \
-//   b     c
-//  / \     \
-// d   e     f
-
-console.log(flattenTree(a)) // ->
-//      a
-//       \
-//        b
-//         \
-//          d
-//           \
-//            e
-//            \
-//             c
-//              \
-//               f
 
 module.exports = {
   flattenTree,
