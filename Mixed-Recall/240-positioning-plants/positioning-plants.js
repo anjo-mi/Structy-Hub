a9120185c38672f6e5aef8d507c6379b7a54622c
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-17 11:43:44 -0400

    Solved 'positioning plants' (node)

---
+++ Mixed-Recall/240-positioning-plants/positioning-plants.js
@@ -0,0 +1,21 @@
+const positioningPlants = (costs, i = 0 , prev = null, memo = {}) => {
+  if (i === costs.length) return 0;
+  const k = i + ';' + prev;
+  if (k in memo) return memo[k];
+  const possiblePlants = costs[i];
+  let min = Infinity;
+  for (let ind = 0 ; ind < possiblePlants.length ; ind++){
+    if (ind !== prev){
+      const cost = possiblePlants[ind];
+      min = Math.min(
+        min,
+        positioningPlants(costs,i+1, ind, memo) + cost,
+      );
+    }
+  }
+  return memo[k] = min;
+};
+
+module.exports = {
+  positioningPlants,
+};
