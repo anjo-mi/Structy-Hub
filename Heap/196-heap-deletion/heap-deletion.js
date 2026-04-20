4549f4d3dc3181834faca19e79cce6bf3337b711
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-20 11:21:53 -0400

    Solved 'heap deletion' (node)

---
+++ Heap/196-heap-deletion/heap-deletion.js
@@ -0,0 +1,64 @@
+class MinHeap {
+  constructor() {
+    this.array = [];
+  }
+
+  isEmpty() {
+    return this.array.length === 0;
+  }
+
+  size() {
+    return this.array.length;
+  }
+
+  swap(idx1, idx2) {
+    [this.array[idx1], this.array[idx2]] = [this.array[idx2], this.array[idx1]];
+  }
+
+  siftUp(idx) {
+    let currentIdx = idx;
+    while (currentIdx > 0) {
+      const parentIdx = Math.floor((currentIdx - 1) / 2);
+      if (this.array[currentIdx] < this.array[parentIdx]) {
+        this.swap(currentIdx, parentIdx);
+        currentIdx = parentIdx;
+      } else {
+        break;
+      } 
+    }
+  }
+
+  siftDown(i){
+    let ci = i;
+    while (ci < this.array.length - 1){
+      const cv = this.array[ci];
+      const lpi = ci * 2 + 1;
+      const rpi = ci * 2 + 2;
+      const lpv = this.array[lpi] ? this.array[lpi] : Infinity;
+      const rpv = this.array[rpi] ? this.array[rpi] : Infinity;
+      const min = Math.min(lpv,rpv);
+      const mi = min === lpv ? lpi : rpi;
+      if (cv > min) {
+        this.swap(ci,mi);
+        ci = mi;
+      }else break;
+    }
+  }
+
+  insert(val) {
+    this.array.push(val);
+    this.siftUp(this.size() - 1);
+  }
+
+  extractMin() {
+    if (!this.array.length) return null;
+    const first = this.array[0];
+    this.array[0] = this.array.pop();
+    this.siftDown(0);
+    return first;
+  }
+}
+
+module.exports = {
+  MinHeap,
+};
