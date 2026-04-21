9546f39af4859f08631303b7da985c1c98f4cd9b
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-21 11:41:21 -0400

    Solved 'is tree balanced' (node)

--- Binary-Tree-II/192-is-tree-balanced/is-tree-balanced.js
+++ Binary-Tree-II/192-is-tree-balanced/is-tree-balanced.js
@@ -21,50 +21,6 @@ const isTreeBalanced = (r) => {
 };
 
 
-
-const s = new Node("s");
-const t = new Node("t");
-const u = new Node("u");
-const v = new Node("v");
-const x = new Node("x");
-const y = new Node("y");
-s.left = t;
-s.right = u;
-t.left = v;
-u.right = x;
-v.left = y;
-console.log(isTreeBalanced(s), false);
-//        s
-//      /   \
-//     t     u
-//    /      \
-//   v        x
-//  /
-// y
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
-const g = new Node("g");
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-e.left = g;
-console.log(isTreeBalanced(a), true);
-//      a
-//    /   \
-//   b     c
-//  / \     \
-// d   e     f
-//    /
-//   g
-
-
-
 module.exports = {
   isTreeBalanced,
 };
