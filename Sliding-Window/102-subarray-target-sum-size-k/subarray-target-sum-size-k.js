4b92899fc4bd0a7cd9969ce04fe9f8404903c0d9
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-24 19:02:39 -0400

    Solved 'subarray target sum size k' (node)

--- Sliding-Window/102-subarray-target-sum-size-k/subarray-target-sum-size-k.js
+++ Sliding-Window/102-subarray-target-sum-size-k/subarray-target-sum-size-k.js
@@ -2,7 +2,6 @@ const subarrayTargetSumSizeK = (nums, target, k) => {
   const res = [];
   let currSum = nums.slice(0,k).reduce((a,el) => a + el,0);
   for (let i = 0 ; i <= nums.length - k ; i++){
-    // console.log({i,k,currSum})
     if (currSum === target) res.push(nums.slice(i,i+k));
     if (i + k === nums.length) break;
     currSum -= nums[i];
