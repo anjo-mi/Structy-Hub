0efa77b572ea47f79769f58611ad18b0d0975795
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-25 19:29:16 -0400

    Solved 'max subarray product size k' (node)

--- Sliding-Window/099-max-subarray-product-size-k/max-subarray-product-size-k.js
+++ Sliding-Window/099-max-subarray-product-size-k/max-subarray-product-size-k.js
@@ -2,11 +2,8 @@ const maxSubarrayProductSizeK = (nums, k) => {
   let maxProd = nums.slice(0,k).reduce((a,el) => a * el, 1);
   let currProd = maxProd;
   for (let i = 0 ; i < nums.length - k ; i++){
-    console.log({currProd})
     currProd /= nums[i];
-    console.log({currProd})
     currProd *= nums[i+k];
-    console.log({currProd})
     maxProd = Math.max(currProd,maxProd);
   }
   return maxProd;
