5bf6a2a73ae7abcc1a612de201bf49c322673be8
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-29 00:21:56 -0400

    Solved 'find subarray sum' (node)

--- Sliding-Window/105-find-subarray-sum/find-subarray-sum.js
+++ Sliding-Window/105-find-subarray-sum/find-subarray-sum.js
@@ -3,8 +3,6 @@ const findSubarraySum = (nums, target) => {
   let curr = nums[front];
   if (curr === target) return [0];
   for (let i = 1 ; i < nums.length ; i++){
-    // console.log({curr,front,i})
-    // if (curr === target) return [front,i-1];
     while(curr > target){
       curr -= nums[front];
       front++;
@@ -15,7 +13,7 @@ const findSubarraySum = (nums, target) => {
   }
   return [-1,-1];
 };
-console.log(findSubarraySum([3, 1, 4, 9, 2, 1, 7], 10))
+
 module.exports = {
   findSubarraySum,
 };
